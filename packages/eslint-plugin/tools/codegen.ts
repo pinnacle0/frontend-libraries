c3cf9ed81ee5be4e2aaf0be978aa1f9b4d21559c
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-10-09 15:13:39 +0800

    [REFACTOR]: eslint-plugin: Refactor codegen script

---
+++ packages/eslint-plugin/tools/codegen.ts
@@ -0,0 +1,92 @@
+import * as fs from "fs";
+import * as path from "path";
+import {paths} from "./paths";
+import {createPrint, kebabToCamelCase} from "./util";
+
+const {srcDirectory, toolsDirectory} = paths;
+const print = createPrint("codegen");
+
+export default function codegen() {
+    codegenRulesIndex();
+    codegenESLintConfig();
+}
+
+function scanCustomRules() {
+    print.task("Scanning rule definitions at src/rules/");
+    const rulesDirectory = path.join(srcDirectory, "rules/");
+    return fs
+        .readdirSync(rulesDirectory)
+        .map(file => {
+            const fileBasename = path.basename(file, ".ts");
+            console.info(`- ${fileBasename}`);
+            return {kebab: fileBasename, camel: kebabToCamelCase(fileBasename)};
+        })
+        .filter(_ => _.kebab !== "index")
+        .sort((a, b) => a.camel.localeCompare(b.camel));
+}
+
+function codegenRulesIndex() {
+    const rulesIndexFile = path.join(srcDirectory, "rules/index.ts");
+    if (fs.existsSync(rulesIndexFile) && fs.statSync(rulesIndexFile).isFile()) {
+        print.task("Removing src/rules/index.ts");
+        fs.unlinkSync(rulesIndexFile);
+    }
+
+    const allRules = scanCustomRules();
+
+    const output = (function () {
+        print.task("Generating src/rules/index.ts");
+        const importStatements = allRules
+            .map(_ => {
+                return `import {rule as ${_.camel}} from "./${_.kebab}";`;
+            })
+            .join("\n");
+        const ruleDefinitions = allRules
+            .map(_ => {
+                return `"${_.kebab}": ${_.camel},`;
+            })
+            .join("\n    ");
+        const templateFile = path.join(toolsDirectory, "./templates/rules-index.ts");
+        return fs
+            .readFileSync(templateFile, {
+                encoding: "utf8",
+            })
+            .replace("// {{TEMPLATE_IMPORT_STATEMENTS}}", importStatements)
+            .replace("// {{TEMPLATE_RULE_DEFINITIONS}}", ruleDefinitions);
+    })();
+
+    fs.writeFileSync(rulesIndexFile, output, {encoding: "utf8"});
+}
+
+function codegenESLintConfig() {
+    const templateFiles = {
+        baseline: path.join(toolsDirectory, "./templates/config-baseline.ts"),
+        jest: path.join(toolsDirectory, "./templates/config-jest.ts"),
+    };
+
+    const allRules = scanCustomRules();
+
+    for (const configName of ["baseline", "jest"] as const) {
+        const configFile = path.join(srcDirectory, `config/${configName}.ts`);
+        if (fs.existsSync(configFile) && fs.statSync(configFile).isFile()) {
+            print.task(`Removing config/${configName}.ts`);
+            fs.unlinkSync(configFile);
+        }
+
+        const output = (function () {
+            print.task(`Generating config/${configName}.ts`);
+            const ruleDefinitions = allRules
+                .map(_ => {
+                    return `"@pinnacle0/${_.kebab}": ["error"],`;
+                })
+                .join("\n        ");
+            return fs
+                .readFileSync(templateFiles[configName], {
+                    encoding: "utf8",
+                })
+                .replace("// {{TEMPLATE_RULE_DEFINITIONS}}", ruleDefinitions);
+        })();
+
+        fs.writeFileSync(configFile, output, {encoding: "utf8"});
+    }
+}
--- packages/eslint-plugin/tools/export-rules-and-config.ts
+++
@@ -1,7 +0,0 @@
-import {pathMap} from "../config/path-map";
-import * as commands from "./commands";
-
-const {srcConfigDirectory, srcRuleDirectory, templateDirectory} = pathMap;
-
-new commands.GenerateExportRulesFile({srcRuleDirectory, templateDirectory}).run();
-new commands.GenerateConfigFile({srcConfigDirectory, srcRuleDirectory, templateDirectory}).run();
