dab8e56aea90b66a805d60f4e3dfc0c96a7cbf38
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-10-12 11:44:27 +0800

    [REFACTOR]: eslint-plugin: Refactor jest config

--- packages/eslint-plugin/config/jest.config.js
+++ packages/eslint-plugin/config/jest.config.js
@@ -8,8 +8,18 @@ const config = {
     // A preset that is used as a base for Jest's configuration
     preset: "ts-jest",
 
+    // A set of global variables that need to be available in all test environments
+    globals: {
+        "ts-jest": {
+            tsConfig: "<rootDir>/tsconfig.json",
+        },
+    },
+
     // The root directory that Jest should scan for tests and modules within
-    rootDir: "../test",
+    rootDir: "../",
+
+    // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+    testPathIgnorePatterns: ["/node_modules/", "<rootDir>/dist/"],
 
     // A map from regular expressions to paths to transformers
     transform: {
