473eca2dcfca7bed72c369f885af6760b49510e7
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-10-12 11:05:14 +0800

    [REFACTOR]: eslint-plugin: Refactor tools

--- packages/eslint-plugin/tools/util/print-util.ts
+++ packages/eslint-plugin/tools/util/print-util.ts
@@ -1,15 +1,15 @@
 import chalk from "chalk";
 
-const curriedPrint = (descriptiveTitle: string) => (tag: "info" | "task" | "error") => (text: string | Error | Array<string | Error>) => {
-    const emoji = {info: "ℹ️", task: "🛠", error: "❌"}[tag];
-    const color = ({info: "blueBright", task: "greenBright", error: "redBright"} as const)[tag];
-    const body = chalk.whiteBright.bgBlack((Array.isArray(text) ? text : [text]).map(_ => _.toString()).join(" "));
-    console.info("");
-    console.info(chalk[color].bold(`${emoji}  [${descriptiveTitle}]`), body);
+const curriedPrint = (emoji: string) => (color: typeof chalk["Color"]) => {
+    return (descriptiveTitle: string) => (text: string | Error | Array<string | Error>) => {
+        const body = chalk.whiteBright.bgBlack((Array.isArray(text) ? text : [text]).map(_ => _.toString()).join(" "));
+        console.info("");
+        console.info(chalk[color].bold(`${emoji}  [${descriptiveTitle}]`), body);
+    };
 };
 
 export const createPrint = (descriptiveTitle: string) => ({
-    info: curriedPrint(descriptiveTitle)("info"),
-    task: curriedPrint(descriptiveTitle)("task"),
-    error: curriedPrint(descriptiveTitle)("error"),
+    info: curriedPrint("ℹ️")("blueBright")(descriptiveTitle),
+    task: curriedPrint("🛠")("greenBright")(descriptiveTitle),
+    error: curriedPrint("❌")("redBright")(descriptiveTitle),
 });
