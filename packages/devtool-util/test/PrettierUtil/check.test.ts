ef3f7a3fe07c80329691330a55e2e09cec5a9545
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-11-04 15:25:29 +0800

    [REFACTOR]: devtool-util: Refactor test tmp directory

--- packages/devtool-util/test/PrettierUtil/check.test.ts
+++ packages/devtool-util/test/PrettierUtil/check.test.ts
@@ -2,47 +2,59 @@ import * as fs from "fs";
 import * as path from "path";
 import {PrettierUtil} from "../../src";
 
-const formattedFixtureContents = `//
-export function professor() {
-  console.info('is gae');
-}
-`;
-
-const unformattedFixtureContents = `//
-export function professor ()
-{
-    console.info("is gae")
-};
-`;
-
-const fixtureDirectory = path.join(__dirname, "./__tmp_fixtures__");
-const prettierrcFile = path.join(__dirname, "./__tmp_fixtures__/.prettierrc");
-const formattedFixture = path.join(__dirname, "./__tmp_fixtures__/formatted.ts");
-const unformattedFixture = path.join(__dirname, "./__tmp_fixtures__/unformatted.ts");
+const tmpDirectory = path.join(__dirname, "./__tmp__/check");
+const formattedFixturePath = path.join(tmpDirectory, "./formatted.ts");
+const unformattedFixturePath = path.join(tmpDirectory, "./unformatted.ts");
 
 describe("PrettierUtil.check", () => {
     beforeAll(async () => {
-        if (!fs.existsSync(fixtureDirectory)) await fs.promises.mkdir(fixtureDirectory);
-        await Promise.all([
-            fs.promises.writeFile(prettierrcFile, JSON.stringify({semi: true, singleQuote: true, tabWidth: 2}, null, 2), {encoding: "utf8"}),
-            fs.promises.writeFile(formattedFixture, formattedFixtureContents, {encoding: "utf8"}),
-            fs.promises.writeFile(unformattedFixture, unformattedFixtureContents, {encoding: "utf8"}),
-        ]);
+        if (!fs.existsSync(tmpDirectory)) await fs.promises.mkdir(tmpDirectory);
+        await Promise.all(
+            getFixtures().map(({path, data}) => {
+                return fs.promises.writeFile(path, data, {encoding: "utf8"});
+            })
+        );
     });
 
     afterAll(async () => {
-        await fs.promises.rm(fixtureDirectory, {recursive: true, force: true});
+        await fs.promises.rm(tmpDirectory, {recursive: true, force: true});
     });
 
     test("passes when called with formatted file", () => {
-        expect(() => PrettierUtil.check(formattedFixture)).not.toThrowError();
+        expect(() => PrettierUtil.check(formattedFixturePath)).not.toThrowError();
     });
 
     test("throws when called with unformatted file", () => {
-        expect(() => PrettierUtil.check(unformattedFixture)).toThrowError();
+        expect(() => PrettierUtil.check(unformattedFixturePath)).toThrowError();
     });
 
     test("throws when called with directory containing unformatted files", () => {
-        expect(() => PrettierUtil.check(fixtureDirectory)).toThrowError();
+        expect(() => PrettierUtil.check(tmpDirectory)).toThrowError();
     });
 });
+
+function getFixtures(): {path: string; data: string}[] {
+    return [
+        {
+            path: path.join(tmpDirectory, "./.prettierrc"),
+            data: JSON.stringify({semi: true, singleQuote: true, tabWidth: 2}, null, 2),
+        },
+        {
+            path: formattedFixturePath,
+            data: `//
+export function fn() {
+  console.info('is prettier this file is formatted');
+}
+`,
+        },
+        {
+            path: unformattedFixturePath,
+            data: `//
+export function fn ()
+{
+    console.info("is ugly and unformatted")
+};
+`,
+        },
+    ];
+}
