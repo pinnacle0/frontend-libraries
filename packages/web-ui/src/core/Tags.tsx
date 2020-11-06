6b85f8029075e6f4201ea3c85fd2b8539926457e
dionshi <dion.shi.hk@gmail.com>
2020-11-06 18:20:38 +0800

    [REFACTOR]: web-ui: Refactor Tags props

--- packages/web-ui/src/core/Tags.tsx
+++ packages/web-ui/src/core/Tags.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Tag from "antd/lib/tag";
 import "antd/lib/tag/style";
+import {PresetColorType, PresetStatusColorType} from "antd/lib/_util/colors";
+
+type TagColor = PresetColorType | PresetStatusColorType;
 
 export interface Props {
-    items: string[];
+    items: React.ReactChild[];
     /**
      * If undefined, tags are not closable.
      */
     onClose?: (index: number) => void;
-    status?: "success" | "error" | "default";
+    color?: TagColor | ((item: React.ReactChild, index: number) => TagColor);
     maxWidth?: number;
 }
 
@@ -17,11 +20,11 @@ export class Tags extends React.PureComponent<Props> {
 
     private readonly tagStyle: React.CSSProperties = {marginRight: 5, marginBottom: 4};
 
-    renderTag = (item: string, index: number) => {
-        const {status, onClose} = this.props;
-        const color = status === "success" ? "blue" : status === "error" ? "red" : undefined;
+    renderTag = (item: React.ReactChild, index: number) => {
+        const {color, onClose} = this.props;
+        const tagColor = typeof color === "function" ? color(item, index) : color;
         return (
-            <Tag color={color} key={index} style={this.tagStyle} closable={onClose !== undefined} onClose={() => onClose?.(index)}>
+            <Tag color={tagColor} key={index} style={this.tagStyle} closable={onClose !== undefined} onClose={() => onClose?.(index)}>
                 {item}
             </Tag>
         );
@@ -29,6 +32,6 @@ export class Tags extends React.PureComponent<Props> {
 
     render() {
         const {items, maxWidth} = this.props;
-        return maxWidth ? <div style={{maxWidth}}>{items.map(this.renderTag)}</div> : items.map(this.renderTag);
+        return <div style={{maxWidth}}>{items.map(this.renderTag)}</div>;
     }
 }
