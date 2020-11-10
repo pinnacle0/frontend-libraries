de8f1a43d1585c971763d4c940b1b114aafbdc62
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-11-10 12:26:32 +0800

    [REFACTOR]: webpack-util: Refactor build script

--- packages/webpack-util/tools/build.ts
+++ packages/webpack-util/tools/build.ts
@@ -1,15 +1,18 @@
+import {PrettierUtil} from "@pinnacle0/devtool-util/src/PrettierUtil";
+import {Utility} from "@pinnacle0/devtool-util/src/Utility";
+import fs from "fs";
 import path from "path";
-import {runCommand} from "../src/util";
-import checkFormat from "./check-format";
 
-const workspaceRootDirectory = path.join(__dirname, "../../..");
 const projectDirectory = path.join(__dirname, "..");
 
 export default function build() {
-    // TODO: use PrettierUtil.check
-    checkFormat();
-    runCommand(projectDirectory)(
-        String.raw`yarn \
-        tsc --project tsconfig.json`
-    );
+    PrettierUtil.check(path.join(projectDirectory, "src"));
+    PrettierUtil.check(path.join(projectDirectory, "tools"));
+
+    const distDirectory = path.join(projectDirectory, "dist");
+    if (fs.existsSync(distDirectory)) {
+        fs.rmdirSync(distDirectory, {recursive: true});
+    }
+
+    Utility.runCommand("tsc", ["--project", path.join(projectDirectory, "tsconfig.json")]);
 }
