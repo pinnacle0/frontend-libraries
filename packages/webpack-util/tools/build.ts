86f3cd0e072b4e07670d8c95e958138b7357935d
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-11-11 10:46:56 +0800

    [REFACTOR]: webpack-util: Update build script logging

--- packages/webpack-util/tools/build.ts
+++ packages/webpack-util/tools/build.ts
@@ -3,16 +3,23 @@ import {Utility} from "@pinnacle0/devtool-util/src/Utility";
 import fs from "fs";
 import path from "path";
 
+const print = Utility.createConsoleLogger("build");
+
 const projectDirectory = path.join(__dirname, "..");
 
 export default function build() {
+    print.task("Checking code styles");
     PrettierUtil.check(path.join(projectDirectory, "src"));
     PrettierUtil.check(path.join(projectDirectory, "tools"));
 
     const distDirectory = path.join(projectDirectory, "dist");
     if (fs.existsSync(distDirectory)) {
+        print.task("Removing dist directory");
         fs.rmdirSync(distDirectory, {recursive: true});
     }
 
+    print.task("Compiling...");
     Utility.runCommand("tsc", ["--project", path.join(projectDirectory, "tsconfig.json")]);
+
+    print.info("Build successful");
 }
