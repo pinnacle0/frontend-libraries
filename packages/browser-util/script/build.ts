7141e06c77fa5bbee947c655b6eb5db87f1c7a07
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-11-13 12:04:38 +0800

    [CHORE]: browser-util: Add scripts

---
+++ packages/browser-util/script/build.ts
@@ -0,0 +1,23 @@
+import {PrettierUtil, Utility} from "@pinnacle0/devtool-util";
+import path from "path";
+import yargs from "yargs";
+
+const print = Utility.createConsoleLogger("build");
+const isFastMode = yargs.argv.mode === "fast";
+
+if (isFastMode) {
+    print.info("Fast mode enabled, skipping format checking and testing");
+} else {
+    print.task("Checking code styles");
+    PrettierUtil.check(path.join(__dirname, "../src"));
+    PrettierUtil.check(path.join(__dirname, "../script"));
+    PrettierUtil.check(path.join(__dirname, "../test"));
+
+    print.task("Running tests");
+    Utility.runCommand("jest", ["--config", path.join(__dirname, "../config/jest.config.js"), "--bail"]);
+}
+
+print.task("Compiling...");
+Utility.runCommand("tsc", ["--project", path.join(__dirname, "../config/tsconfig.src.json")]);
+
+print.info("Build successful");
