4ed177c452ac8b04b82a917763c2a376837ab6e3
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-11-17 15:53:20 +0800

    [CHORE]: eslint-plugin: Add todo

--- packages/eslint-plugin/src/rules/order-stylesheet-import-statement-last.ts
+++ packages/eslint-plugin/src/rules/order-stylesheet-import-statement-last.ts
@@ -4,6 +4,14 @@ export type MessageIds = "orderStylesheetImportStatementLast";
 
 export const name = "order-stylesheet-import-statement-last";
 
+// TODO/Jamyth: Implement rule "order-stylesheet-import-statement-last"
+// 1) Add a descriptive message to `meta.messages.orderStylesheetImportStatementLast`
+// 2) Report error by calling `context.report` in visitor function
+// 3) Add fixer when calling `context.report` (Remove the incorrectly ordered import statement, append it after the last import statement)
+//    Note that you can "mutate" the source code multiple times by returning an array of `fixer` calls.
+//    See "module-class-method-decorators" for reference.
+// 4) Make sure the test case passes (Or update the test cases if necessary); see `order-stylesheet-import-statement-last.test.ts`
+// 5) Remove these comments
 export const rule = ESLintUtils.RuleCreator(name => name)<[], MessageIds>({
     name,
     meta: {
--- packages/eslint-plugin/test/rules/order-stylesheet-import-statement-last.test.ts
+++ packages/eslint-plugin/test/rules/order-stylesheet-import-statement-last.test.ts
@@ -6,6 +6,16 @@ const messageId: MessageIds = "orderStylesheetImportStatementLast";
 
 const ruleTester = new TSESLint.RuleTester(createConfig());
 
+// TODO/Jamyth: Make sure tests passes for rule "order-stylesheet-import-statement-last"
+// To run this test file individually in watch mode (without running other test files), use the following command:
+// ```sh
+// $ cd packages/eslint-plugin/
+// $ yarn jest test/rules/order-stylesheet-import-statement-last.test.ts --config config/jest.config.js --watch
+// ```
+//
+// 1) Add/remove `valid` and `invalid` test cases if you think they do not describe the behaviour of this lint rule.
+// 2) Note that fixers are **whitespace-sensitive** so I dedented the code snippets so it is easier to create the fixer.
+// 3) Remove these comments
 ruleTester.run(name, rule, {
     valid: [
         `import React from "react";
