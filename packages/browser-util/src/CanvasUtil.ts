a7bafb9c9fc282cd67b47edadeb4a3b847d75fc0
timsheung <timsheung1995>
2020-11-18 10:36:51 +0800

    [FIX]: browser-util: 0.0.3 Fix CanvasUtil

--- packages/browser-util/src/CanvasUtil.ts
+++ packages/browser-util/src/CanvasUtil.ts
@@ -2,12 +2,12 @@
  * This function draws line on canvas between the center of nodes.
  *
  * Attention:
- * canvasContext should share the same offsetParent with the nodes.
+ * canvasContext must share the same offsetParent with the nodes.
  */
 function drawPathBetweenNodes(nodes: NodeListOf<Element>, canvasContext: CanvasRenderingContext2D, strokeColor: string) {
     canvasContext.save();
     canvasContext.beginPath();
-    const [first, ...rest] = Array.from(nodes).map(elementCenter);
+    const [first, ...rest] = Array.from(nodes).map(_ => elementCenter(_, canvasContext.canvas.offsetParent));
     if (first) {
         canvasContext.moveTo(first.x, first.y);
         rest.forEach(node => {
@@ -22,12 +22,20 @@ function drawPathBetweenNodes(nodes: NodeListOf<Element>, canvasContext: CanvasR
     canvasContext.restore();
 }
 
-function elementCenter(element: Element | null): {x: number; y: number} | null {
+function elementCenter(element: Element | null, canvasParentElement: Element | null): {x: number; y: number} | null {
     if (element) {
-        const htmlElement = element as HTMLSpanElement;
+        const htmlElement = element as HTMLElement;
+        let x = htmlElement.offsetLeft + htmlElement.offsetWidth / 2;
+        let y = htmlElement.offsetTop + htmlElement.offsetHeight / 2;
+        let parentElement = htmlElement.offsetParent as HTMLElement | null;
+        while (parentElement && canvasParentElement && parentElement !== canvasParentElement) {
+            x = x + parentElement.offsetLeft;
+            y = y + parentElement.offsetTop;
+            parentElement = parentElement.offsetParent as HTMLElement | null;
+        }
         return {
-            x: htmlElement.offsetLeft + htmlElement.offsetWidth / 2,
-            y: htmlElement.offsetTop + htmlElement.offsetHeight / 2,
+            x,
+            y,
         };
     } else {
         return null;
