d63a573ccbd3a1751e82e4a2bbbb4b82c0803fed
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-11-20 18:17:08 +0800

    [REFACTOR]: browser-util: Refactor scripts

--- packages/browser-util/script/build.ts
+++ packages/browser-util/script/build.ts
@@ -1,37 +1,57 @@
-import {PrettierUtil, Utility} from "@pinnacle0/devtool-util/src";
+import {PrettierUtil, TaskRunner, Utility} from "@pinnacle0/devtool-util/src";
+import fs from "fs";
 import path from "path";
-import yargs from "yargs";
 
-const print = Utility.createConsoleLogger("build");
-const isFastMode = yargs.argv.mode === "fast";
 const FilePath = {
     config: path.join(__dirname, "../config"),
-    dist: path.join(__dirname, "../dist"),
+    build: path.join(__dirname, "../build"),
     script: path.join(__dirname, "../script"),
     src: path.join(__dirname, "../src"),
     test: path.join(__dirname, "../test"),
 
     jestConfig: path.join(__dirname, "../config/jest.config.js"),
     tsConfigForSrc: path.join(__dirname, "../config/tsconfig.src.json"),
+    projectPackageJSON: path.join(__dirname, "../package.json"),
+    projectLicense: path.join(__dirname, "../LICENSE.md"),
+    buildPackageJSON: path.join(__dirname, "../build/package.json"),
+    buildLicense: path.join(__dirname, "../build/LICENSE.md"),
 };
 
-if (isFastMode) {
-    print.info("Fast mode enabled, skipping format checking and testing");
-} else {
-    print.task("Checking code styles");
-    PrettierUtil.check(FilePath.config);
-    PrettierUtil.check(FilePath.script);
-    PrettierUtil.check(FilePath.src);
-    PrettierUtil.check(FilePath.test);
-
-    print.task("Running tests");
-    Utility.runCommand("jest", ["--config", FilePath.jestConfig, "--bail"]);
-}
-
-print.task("Preparing dist directory");
-Utility.prepareEmptyDirectory(FilePath.dist);
-
-print.task("Compiling");
-Utility.runCommand("tsc", ["--project", FilePath.tsConfigForSrc]);
-
-print.info("Build successful");
+new TaskRunner("build").execute([
+    {
+        name: "check code styles",
+        skipInFastMode: true,
+        execute: () => {
+            PrettierUtil.check(FilePath.config);
+            PrettierUtil.check(FilePath.script);
+            PrettierUtil.check(FilePath.src);
+            PrettierUtil.check(FilePath.test);
+        },
+    },
+    {
+        name: "test",
+        skipInFastMode: true,
+        execute: () => {
+            Utility.runCommand("jest", ["--config", FilePath.jestConfig, "--bail"]);
+        },
+    },
+    {
+        name: 'prepare "build" directory',
+        execute: () => {
+            Utility.prepareEmptyDirectory(FilePath.build);
+        },
+    },
+    {
+        name: "tsc compile",
+        execute: () => {
+            Utility.runCommand("tsc", ["--project", FilePath.tsConfigForSrc]);
+        },
+    },
+    {
+        name: "copy package.json, markdown files",
+        execute: () => {
+            fs.copyFileSync(FilePath.projectPackageJSON, FilePath.buildPackageJSON);
+            fs.copyFileSync(FilePath.projectLicense, FilePath.buildLicense);
+        },
+    },
+]);
--- packages/browser-util/script/format.ts
+++ packages/browser-util/script/format.ts
@@ -1,6 +1,7 @@
 import {PrettierUtil} from "@pinnacle0/devtool-util/src";
 import path from "path";
 
+PrettierUtil.format(path.join(__dirname, "../config"));
 PrettierUtil.format(path.join(__dirname, "../src"));
 PrettierUtil.format(path.join(__dirname, "../script"));
 PrettierUtil.format(path.join(__dirname, "../test"));
