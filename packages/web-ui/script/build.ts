5e142836be96780b1fefc124b7b3269a6d0de56c
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-11-24 09:28:47 +0800

    [REFACTOR]: web-ui: Refactor build script

--- packages/web-ui/script/build.ts
+++ packages/web-ui/script/build.ts
@@ -1,15 +1,12 @@
-import {PrettierUtil, Utility} from "@pinnacle0/devtool-util/src";
+import {PrettierUtil, TaskRunner, Utility} from "@pinnacle0/devtool-util/src";
 import fs from "fs";
 import {copySync as fsExtraCopySync} from "fs-extra";
 import path from "path";
-import yargs from "yargs";
 
-const print = Utility.createConsoleLogger("build");
-const isFastMode = yargs.argv.mode === "fast";
 const FilePath = {
     project: path.join(__dirname, ".."),
     config: path.join(__dirname, "../config"),
-    dist: path.join(__dirname, "../dist"),
+    build: path.join(__dirname, "../build"),
     script: path.join(__dirname, "../script"),
     src: path.join(__dirname, "../src"),
     test: path.join(__dirname, "../test"),
@@ -19,59 +16,88 @@ const FilePath = {
     projectPackageJSON: path.join(__dirname, "../package.json"),
     projectReadMe: path.join(__dirname, "../README.md"),
     projectLicense: path.join(__dirname, "../LICENSE.md"),
+    buildPackageJSON: path.join(__dirname, "../build/package.json"),
+    buildReadMe: path.join(__dirname, "../build/README.md"),
+    buildLicense: path.join(__dirname, "../build/LICENSE.md"),
 };
 
-if (isFastMode) {
-    print.info("Fast mode enabled, skipping format checking and testing");
-} else {
-    print.task("Checking code styles");
-    PrettierUtil.check(FilePath.config);
-    PrettierUtil.check(FilePath.script);
-    PrettierUtil.check(FilePath.src);
-    PrettierUtil.check(FilePath.test);
-
-    print.task("Checking lint for js/ts");
-    Utility.runCommand("eslint", ["--ext=.js,.jsx,.ts,.tsx", FilePath.src]);
-    Utility.runCommand("eslint", ["--ext=.js,.jsx,.ts,.tsx", FilePath.test]);
-
-    print.task("Checking lint for stylesheets");
-    Utility.runCommand("stylelint", [path.join(FilePath.src, "**/*.{css,less}")]);
-    Utility.runCommand("stylelint", [path.join(FilePath.test, "**/*.{css,less}")]);
-
-    print.task("Running unit tests");
-    Utility.runCommand("jest", ["--config", FilePath.jestConfig, "--bail"]);
-}
-
-print.task("Preparing dist directory");
-Utility.prepareEmptyDirectory(FilePath.dist);
-
-print.task("Compiling");
-Utility.runCommand("tsc", ["--project", FilePath.tsConfigForSrc]);
-
-print.task("Copying assets to dist directory");
-const supportedExtensions = [".less", ".css", ".json", ".jpg", ".png", ".gif", ".mp3", ".mp4", ".wmv"];
-fsExtraCopySync(FilePath.src, FilePath.dist, {
-    filter: source => {
-        const extension = path.extname(source);
-        // source may be a directory or a file under the directory.
-        if (fs.statSync(source).isDirectory()) {
-            return true;
-        } else if (supportedExtensions.includes(extension)) {
-            print.info(`Asset (${extension}) copied from "${path.relative(FilePath.src, source)}"`);
-            return true;
-        } else {
-            if (extension !== ".tsx" && extension !== ".ts") {
-                print.error(`Asset (${extension}) is unsupported, skipped "${path.relative(FilePath.src, source)}"`);
-            }
-            return false;
-        }
+new TaskRunner("build").execute([
+    {
+        name: "check code styles",
+        skipInFastMode: true,
+        execute: () => {
+            PrettierUtil.check(FilePath.config);
+            PrettierUtil.check(FilePath.script);
+            PrettierUtil.check(FilePath.src);
+            PrettierUtil.check(FilePath.test);
+        },
     },
-    dereference: true,
-});
-
-print.task("Copying package.json, markdown files to dist folder");
-fs.copyFileSync(FilePath.projectPackageJSON, path.join(FilePath.dist, "package.json"));
-fs.copyFileSync(FilePath.projectReadMe, path.join(FilePath.dist, "README.md"));
-fs.copyFileSync(FilePath.projectLicense, path.join(FilePath.dist, "LICENSE.md"));
-
-print.info("Build successful");
+    {
+        name: "eslint",
+        skipInFastMode: true,
+        execute: () => {
+            Utility.runCommand("eslint", ["--ext=.js,.jsx,.ts,.tsx", FilePath.src]);
+            Utility.runCommand("eslint", ["--ext=.js,.jsx,.ts,.tsx", FilePath.test]);
+        },
+    },
+    {
+        name: "stylelint",
+        skipInFastMode: true,
+        execute: () => {
+            Utility.runCommand("stylelint", [path.join(FilePath.src, "**/*.{css,less}")]);
+            Utility.runCommand("stylelint", [path.join(FilePath.test, "**/*.{css,less}")]);
+        },
+    },
+    {
+        name: "unit test",
+        skipInFastMode: true,
+        execute: () => {
+            Utility.runCommand("jest", ["--config", FilePath.jestConfig, "--bail"]);
+        },
+    },
+    {
+        name: "prepare build directory",
+        execute: () => {
+            Utility.prepareEmptyDirectory(FilePath.build);
+        },
+    },
+    {
+        name: "compile with tsc",
+        execute: () => {
+            Utility.runCommand("tsc", ["--project", FilePath.tsConfigForSrc]);
+        },
+    },
+    {
+        name: "copy assets to build directory",
+        execute: () => {
+            const print = Utility.createConsoleLogger("build");
+            const supportedExtensions = [".less", ".css", ".json", ".jpg", ".png", ".gif", ".mp3", ".mp4", ".wmv"];
+            fsExtraCopySync(FilePath.src, FilePath.build, {
+                filter: source => {
+                    const extension = path.extname(source);
+                    // source may be a directory or a file under the directory.
+                    if (fs.statSync(source).isDirectory()) {
+                        return true;
+                    } else if (supportedExtensions.includes(extension)) {
+                        print.info(`Asset (${extension}) copied from "${path.relative(FilePath.src, source)}"`);
+                        return true;
+                    } else {
+                        if (extension !== ".tsx" && extension !== ".ts") {
+                            print.error(`Asset (${extension}) is unsupported, skipped "${path.relative(FilePath.src, source)}"`);
+                        }
+                        return false;
+                    }
+                },
+                dereference: true,
+            });
+        },
+    },
+    {
+        name: "copy package.json, markdown files",
+        execute: () => {
+            fs.copyFileSync(FilePath.projectPackageJSON, FilePath.buildPackageJSON);
+            fs.copyFileSync(FilePath.projectReadMe, FilePath.buildReadMe);
+            fs.copyFileSync(FilePath.projectLicense, FilePath.buildLicense);
+        },
+    },
+]);
