2710a1dcc2c4d1184eb38e09bd27099967607999
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-11-24 11:29:38 +0800

    [CHORE]: browser-util: Update jest config

--- packages/browser-util/config/jest.config.js
+++ packages/browser-util/config/jest.config.js
@@ -16,10 +16,10 @@ const config = {
     rootDir: "../",
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-    testPathIgnorePatterns: ["/node_modules/", "<rootDir>/dist/"],
+    testPathIgnorePatterns: ["/node_modules/", "<rootDir>/build/"],
 
     // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
-    watchPathIgnorePatterns: ["<rootDir>/dist/"],
+    watchPathIgnorePatterns: ["<rootDir>/build/"],
 };
 
 module.exports = config;
