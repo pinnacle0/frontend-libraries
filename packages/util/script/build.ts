1e512038a14cabcdd7ed85c45a3eaccfe54e5741
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-11-24 09:20:44 +0800

    [REFACTOR]: util: Refactor build script

--- packages/util/script/build.ts
+++ packages/util/script/build.ts
@@ -1,14 +1,11 @@
-import {PrettierUtil, Utility} from "@pinnacle0/devtool-util/src";
+import {PrettierUtil, TaskRunner, Utility} from "@pinnacle0/devtool-util/src";
 import fs from "fs";
 import path from "path";
-import yargs from "yargs";
 
-const print = Utility.createConsoleLogger("build");
-const isFastMode = yargs.argv.mode === "fast";
 const FilePath = {
     project: path.join(__dirname, ".."),
     config: path.join(__dirname, "../config"),
-    dist: path.join(__dirname, "../dist"),
+    build: path.join(__dirname, "../build"),
     script: path.join(__dirname, "../script"),
     src: path.join(__dirname, "../src"),
     test: path.join(__dirname, "../test"),
@@ -18,37 +15,55 @@ const FilePath = {
     projectPackageJSON: path.join(__dirname, "../package.json"),
     projectReadMe: path.join(__dirname, "../README.md"),
     projectLicense: path.join(__dirname, "../LICENSE.md"),
-    distPackageJSON: path.join(__dirname, "../dist/package.json"),
-    distReadMe: path.join(__dirname, "../dist/README.md"),
-    distLicense: path.join(__dirname, "../dist/LICENSE.md"),
+    buildPackageJSON: path.join(__dirname, "../build/package.json"),
+    buildReadMe: path.join(__dirname, "../build/README.md"),
+    buildLicense: path.join(__dirname, "../build/LICENSE.md"),
 };
 
-if (isFastMode) {
-    print.info("Fast mode enabled, skipping format checking and testing");
-} else {
-    print.task("Check code style");
-    PrettierUtil.check(FilePath.config);
-    PrettierUtil.check(FilePath.script);
-    PrettierUtil.check(FilePath.src);
-    PrettierUtil.check(FilePath.test);
-
-    print.task("Check lint");
-    Utility.runCommand("eslint", ["--ext=.js,.jsx,.ts,.tsx", FilePath.src]);
-    Utility.runCommand("eslint", ["--ext=.js,.jsx,.ts,.tsx", FilePath.test]);
-
-    print.task("Run tests");
-    Utility.runCommand("jest", ["--config", FilePath.jestConfig, "--bail", "--coverage"]);
-}
-
-print.task("Preparing dist directory");
-Utility.prepareEmptyDirectory(FilePath.dist);
-
-print.task("Compiling");
-Utility.runCommand("tsc", ["--build", FilePath.tsConfigForProjectReferences]);
-
-print.task("Copying package.json and markdown files to dist folder");
-fs.copyFileSync(FilePath.projectPackageJSON, FilePath.distPackageJSON);
-fs.copyFileSync(FilePath.projectReadMe, FilePath.distReadMe);
-fs.copyFileSync(FilePath.projectLicense, FilePath.distLicense);
-
-print.info("Build successful");
+new TaskRunner("build").execute([
+    {
+        name: "check code style",
+        skipInFastMode: true,
+        execute: () => {
+            PrettierUtil.check(FilePath.config);
+            PrettierUtil.check(FilePath.script);
+            PrettierUtil.check(FilePath.src);
+            PrettierUtil.check(FilePath.test);
+        },
+    },
+    {
+        name: "lint",
+        skipInFastMode: true,
+        execute: () => {
+            Utility.runCommand("eslint", ["--ext=.js,.jsx,.ts,.tsx", FilePath.src]);
+            Utility.runCommand("eslint", ["--ext=.js,.jsx,.ts,.tsx", FilePath.test]);
+        },
+    },
+    {
+        name: "test",
+        skipInFastMode: true,
+        execute: () => {
+            Utility.runCommand("jest", ["--config", FilePath.jestConfig, "--bail", "--coverage"]);
+        },
+    },
+    {
+        name: "prepare build directory",
+        execute: () => {
+            Utility.prepareEmptyDirectory(FilePath.build);
+        },
+    },
+    {
+        name: "compile with tsc",
+        execute: () => {
+            Utility.runCommand("tsc", ["--build", FilePath.tsConfigForProjectReferences]);
+        },
+    },
+    {
+        name: "copy package.json, markdown files",
+        execute: () => {
+            fs.copyFileSync(FilePath.projectPackageJSON, FilePath.buildPackageJSON);
+            fs.copyFileSync(FilePath.projectReadMe, FilePath.buildReadMe);
+            fs.copyFileSync(FilePath.projectLicense, FilePath.buildLicense);
+        },
+    },
+]);
