3c28537cbf26af3b3e4ccf43feca8f70cd69c45b
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-12-02 13:47:26 +0800

    [CHORE]: eslint-plugin: Make dogfood faster

--- packages/eslint-plugin/dogfood/baseline.js
+++ packages/eslint-plugin/dogfood/baseline.js
@@ -2,6 +2,7 @@
 
 require("ts-node").register({
     project: require.resolve("../../../config/tsconfig.base.json"),
+    transpileOnly: true,
 });
 
 module.exports = {...require("../src/config/baseline").configBaseline};
