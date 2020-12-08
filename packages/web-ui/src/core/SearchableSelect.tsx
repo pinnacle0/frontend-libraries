bef23482638ddf5eccf84f6c3fe7ed7f863bf3ef
andyyu <andy.yu@pinnacle-holdings.hk>
2020-12-08 16:50:32 +0800

    [REFACTOR]: web-ui: refactor SearchableSelect

--- packages/web-ui/src/core/SearchableSelect.tsx
+++ packages/web-ui/src/core/SearchableSelect.tsx
@@ -1,7 +1,9 @@
 import {Select, Props as SelectProps, SelectValue, SelectOptionProps} from "./Select";
 import React from "react";
 
-export interface Props<ValueType extends SelectValue> extends Omit<SelectProps<ValueType>, "optionFilterProp" | "showSearch"> {}
+export interface Props<ValueType extends SelectValue> extends Omit<SelectProps<ValueType>, "optionFilterProp" | "showSearch" | "options"> {
+    options?: Array<{value: string; label: string}>;
+}
 
 export class SearchableSelect<ValueType extends SelectValue> extends React.PureComponent<Props<ValueType>> {
     static displayName = "SearchableSelect";
@@ -9,6 +11,6 @@ export class SearchableSelect<ValueType extends SelectValue> extends React.PureC
     static Option = Select.Option as React.ComponentType<SelectOptionProps & {children: string}>;
 
     render() {
-        return <Select optionFilterProp="children" showSearch {...this.props} />;
+        return <Select optionFilterProp={this.props.options ? "label" : "children"} showSearch {...this.props} />;
     }
 }
