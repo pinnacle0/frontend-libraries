dfb21f5f747dffcbff12079d051fdb032df3de7b
dionshi <dion.shi.hk@gmail.com>
2020-12-22 16:20:51 +0800

    [REFACTOR]: web-ui: 1.0.0: Update BrowserUtil.newTab to void function, rather than Promise

--- packages/browser-util/src/BrowserUtil.ts
+++ packages/browser-util/src/BrowserUtil.ts
@@ -80,22 +80,17 @@ function scrollTo(position: number, container: Element | null = null) {
  * Resolve when new tab is loaded, only applicable to same domain.
  * Ref: https://stackoverflow.com/questions/3030859/detecting-the-onload-event-of-a-window-opened-with-window-open
  */
-function newTab(url: string, sizeOptions: Partial<BrowserNewTabSizeOptions> = {}): Promise<void> {
-    return new Promise<void>(resolve => {
-        let features: string | undefined;
-        if (Object.keys(sizeOptions).length > 0) {
-            features = "toolbar=no,location=no,status=no,menubar=no,resizable=no" + Object.entries(sizeOptions).map(([key, value]) => `,${key}=${value}`);
-        }
+function newTab(url: string, sizeOptions: Partial<BrowserNewTabSizeOptions> = {}) {
+    let features: string | undefined;
+    if (Object.keys(sizeOptions).length > 0) {
+        features = "toolbar=no,location=no,status=no,menubar=no,resizable=no" + Object.entries(sizeOptions).map(([key, value]) => `,${key}=${value}`);
+    }
 
-        const newWindow = window.open(url, "newTab", features);
-        if (newWindow) {
-            newWindow.addEventListener("load", () => resolve(), true);
-            newWindow.addEventListener("error", () => resolve(), true);
-        } else {
-            // In case some browser blocks popup, fallback to same tab open
-            window.location.href = url;
-        }
-    });
+    const newWindow = window.open(url, "_blank", features);
+    if (!newWindow) {
+        // In case some browser blocks popup, fallback to same tab open
+        window.location.href = url;
+    }
 }
 
 function openQQ(qq: string) {
