f9c005d5a6b6f346d364fc07842639ebf40e9d8a
dionshi <dion.shi.hk@gmail.com>
2020-12-28 18:24:43 +0800

    [REFACTOR]: util: Review MarkAsOptional typing

--- packages/util/src/type.ts
+++ packages/util/src/type.ts
@@ -1,14 +1,16 @@
 import React from "react";
 
-export type KeysOfType<T, ExpectedValueType> = {[P in keyof T]: T[P] extends ExpectedValueType ? P : never}[keyof T];
-
 export type ArrayElement<ArrayType extends readonly unknown[]> = ArrayType[number];
 
-export type RequireFields<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
 export type PickOptional<T> = Pick<T, {[K in keyof T]-?: {} extends {[P in K]: T[K]} ? K : never}[keyof T]>;
 export type PickNonNullable<T> = Pick<T, NonNullableKeys<T>>;
+
+export type KeysOfType<T, ExpectedValueType> = {[P in keyof T]: T[P] extends ExpectedValueType ? P : never}[keyof T];
 export type NullableKeys<T> = {[K in keyof T]: T[K] extends NonNullable<T[K]> ? never : K}[keyof T];
 export type NonNullableKeys<T> = {[K in keyof T]: T[K] extends NonNullable<T[K]> ? K : never}[keyof T];
+
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
+// TODO/Jamyth: OptionalFields, and add some test cases
 export type MarkAsOptional<T, K extends keyof T> = {[P in K]+?: T[P]} & {[P in Exclude<keyof T, K>]: T[P]};
 
 export type SafeReactChild = React.ReactChild | boolean | null;
