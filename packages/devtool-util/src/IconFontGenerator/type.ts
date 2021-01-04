4af0f4e3f7c95b8b44b23a3c881277bd3114b47f
dionshi <dion.shi.hk@gmail.com>
2021-01-04 17:42:35 +0800

    [REFACTOR]: devtool-util: Remove unused TODO

--- packages/devtool-util/src/IconFontGenerator/type.ts
+++ packages/devtool-util/src/IconFontGenerator/type.ts
@@ -1,7 +1,6 @@
 export interface WebIconFontGeneratorOptions {
     iconComponentDirectory: string;
     staticDirectory: string;
-    // TODO/dion: do not remove this now, i will check again
     fontFamily?: string;
 }
 
