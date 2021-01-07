75a7e68286e5144d8328923a094d5de5b7f7d8fd
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-01-07 11:51:30 +0800

    [FEATURE]: devtool-util: PrettierUtil auto resolve prettierignore file

--- packages/devtool-util/src/PrettierUtil.ts
+++ packages/devtool-util/src/PrettierUtil.ts
@@ -5,6 +5,21 @@ import {Utility} from "./Utility";
 // https://prettier.io/docs/en/cli.html#exit-codes
 const PRETTIER_EXIT_CODE_WHEN_NO_FILES_ARE_FOUND = 2;
 
+// https://github.com/prettier/prettier/pull/6203#issuecomment-702319765
+function findFirstPrettierIgnoreFile(searchPath: string): string | null {
+    const prettierIgnorePath = path.join(searchPath, ".prettierignore");
+    if (fs.existsSync(prettierIgnorePath) && fs.statSync(prettierIgnorePath).isFile()) {
+        return prettierIgnorePath;
+    }
+
+    const parentDirectory = path.dirname(searchPath);
+    if (parentDirectory === searchPath) {
+        return null;
+    }
+
+    return findFirstPrettierIgnoreFile(parentDirectory);
+}
+
 function runPrettierCommand(fileOrDirectory: string, flag: "--check" | "--write") {
     if (fileOrDirectory.includes("*") || fileOrDirectory.includes("{")) {
         throw new Error(`Glob pattern not supported: ${fileOrDirectory}`);
@@ -13,11 +28,14 @@ function runPrettierCommand(fileOrDirectory: string, flag: "--check" | "--write"
         throw new Error(`Path not exist: ${fileOrDirectory}`);
     }
 
+    const prettierIgnorePath = findFirstPrettierIgnoreFile(fileOrDirectory);
+    const prettierIgnoreFlags = prettierIgnorePath ? ["--ignore-path", prettierIgnorePath] : [];
+
     const fileStats = fs.statSync(fileOrDirectory);
     if (fileStats.isDirectory()) {
         const quotedGlobPattern = path.join(fileOrDirectory, `"**/*.{css,html,js,json,jsx,less,ts,tsx}"`);
         try {
-            Utility.runCommand("prettier", [flag, quotedGlobPattern]);
+            Utility.runCommand("prettier", [flag, quotedGlobPattern, ...prettierIgnoreFlags]);
         } catch (error) {
             if (typeof error === "object" && error !== null && error?.childProcessResult?.status === PRETTIER_EXIT_CODE_WHEN_NO_FILES_ARE_FOUND) {
                 return;
@@ -25,7 +43,7 @@ function runPrettierCommand(fileOrDirectory: string, flag: "--check" | "--write"
             throw error;
         }
     } else if (fileStats.isFile()) {
-        Utility.runCommand("prettier", [flag, fileOrDirectory]);
+        Utility.runCommand("prettier", [flag, fileOrDirectory, ...prettierIgnoreFlags]);
     } else {
         throw new Error(`Path is not a file/directory: ${fileOrDirectory}`);
     }
