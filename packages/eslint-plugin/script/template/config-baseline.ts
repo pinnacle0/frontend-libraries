48521ac54334c63a41229ce12104bc5bf23fc240
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-01-12 14:42:20 +0800

    [FEATURE]: eslint-plugin: Turn off no-var-requires rule

--- packages/eslint-plugin/script/template/config-baseline.ts
+++ packages/eslint-plugin/script/template/config-baseline.ts
@@ -78,6 +78,7 @@ export const configBaseline: ESLint.Linter.Config = {
         "@typescript-eslint/no-non-null-assertion": "off",
         "@typescript-eslint/no-unused-vars": "off",
         "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/no-var-requires": "off",
 
         "eslint-comments/no-unlimited-disable": ["error"],
         "eslint-comments/require-description": [
