4393bd91f0a5c193630720c67ad5fda81cc2e8f1
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-01-15 15:51:28 +0800

    [REFACTOR]: root: No more eslint dogfood

--- packages/eslint-plugin/dogfood/baseline.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env node */
-
-require("ts-node").register({
-    project: require.resolve("../../../config/tsconfig.base.json"),
-    transpileOnly: true,
-});
-
-module.exports = {...require("../src/config/baseline").baseline};
