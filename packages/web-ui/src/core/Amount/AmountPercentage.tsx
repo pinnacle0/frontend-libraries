1e037935c376c06c75e6671ae2c185297a74fb1d
dionshi <dion.shi.hk@gmail.com>
2021-01-19 18:33:36 +0800

    [REFACTOR]: web-ui: Update AmountPercentage value as optional

--- packages/web-ui/src/core/Amount/AmountPercentage.tsx
+++ packages/web-ui/src/core/Amount/AmountPercentage.tsx
@@ -8,19 +8,16 @@ interface BaseProps extends Omit<AmountProps, OmitPropsKeys> {}
 
 interface OwnProps {
     /**
+     * Value of display amount in decimal, e.g: 0.2 for 20%.
+     */
+    value?: number | null;
+    /**
      * Number of decimal points used for **percentage** values.
      *
      * To use 2 decimal points (eg 12.34%) in percentage values, `props.percentageScale` should be `2`.
      * Must be an integer, otherwise throws an Error.
      */
     percentageScale: number;
-
-    /**
-     * Value of display amount in **decimal** number.
-     *
-     * If the value is `20%`, then `props.value` should be `0.2`.
-     */
-    value: number | null;
 }
 
 export interface Props extends BaseProps, OwnProps {}
@@ -30,7 +27,7 @@ export class AmountPercentage extends React.PureComponent<Props> {
 
     render() {
         const {percentageScale, value, ...restProps} = this.props;
-        const percentageValue = value !== null ? value * 100 : value;
+        const percentageValue = value !== null && value !== undefined ? value * 100 : value;
         return <Amount value={percentageValue} scale={percentageScale} postfix="%" {...restProps} />;
     }
 }
