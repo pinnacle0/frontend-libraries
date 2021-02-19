3ecdb4d6f620757ea2e04c7f920bbfa7a0e21242
Jamyth Luk <jamythluk@Jamyths-MacBook-Pro.local>
2021-02-19 09:25:22 +0800

    [REFACTOR]: devtool-util: refactor version checker, with much cleaner codes

--- packages/devtool-util/src/VersionChecker.ts
+++ packages/devtool-util/src/VersionChecker.ts
@@ -24,9 +24,7 @@ interface VersionCheckerOptions {
 export class VersionChecker {
     private readonly projectDirectory: string;
     private readonly skipLibs: string[];
-    private packages: {
-        [key: string]: string;
-    } = {};
+    private packages: Record<string, string> = {};
 
     constructor({projectDirectory, skipLibs}: VersionCheckerOptions) {
         this.projectDirectory = projectDirectory;
@@ -38,48 +36,38 @@ export class VersionChecker {
             {
                 name: "Resolve package.json",
                 skipInFastMode: true,
-                execute: this.resolvePackageJson,
+                execute: () => {
+                    if (!fs.existsSync(this.projectDirectory + "/package.json")) throw new Error(`Package.json does not exist in [${this.projectDirectory}]`);
+                },
             },
             {
                 name: "Extract Dependencies",
-                execute: this.extractDependencies,
+                execute: () => {
+                    const packageJson = fs.readFileSync(this.projectDirectory + "/package.json");
+                    const json = JSON.parse(packageJson.toString("utf-8"));
+                    const packages = {
+                        ...json.devDependencies,
+                        ...json.dependencies,
+                    };
+                    this.skipLibs.forEach(packageName => {
+                        delete packages[packageName];
+                    });
+                    this.packages = packages;
+                },
             },
             {
                 name: "Validate Package Versions in node_modules",
-                execute: this.validatePackageVersion,
+                execute: () => {
+                    Object.entries(this.packages).map(([packageName, version]) => {
+                        const packagePath = require.resolve(`${packageName}/package.json`, {
+                            paths: [this.projectDirectory],
+                        });
+                        const packageJson = require(packagePath);
+                        const latest_version = packageJson.version;
+                        if (version !== latest_version) throw new Error(`Package version is not match. [${packageName}]: ${version} -> ${latest_version}`);
+                    });
+                },
             },
         ]);
-    }
-
-    private resolvePackageJson() {
-        if (!fs.existsSync(this.projectDirectory + "/package.json")) throw new Error(`Package.json does not exist in [${this.projectDirectory}]`);
-    }
-
-    private extractDependencies() {
-        const packageJson = fs.readFileSync(this.projectDirectory + "/package.json");
-        const json = JSON.parse(packageJson.toString("utf-8"));
-        const packages = {
-            ...json.devDependencies,
-            ...json.dependencies,
-        };
-        this.skipLibs.forEach(packageName => {
-            delete packages[packageName];
-        });
-        this.packages = packages;
-    }
-
-    private validatePackageVersion() {
-        Object.entries(this.packages).map(([packageName, version]) => {
-            const latest_version = this.getVersion(packageName);
-            if (version !== latest_version) throw new Error(`Package version is not match. [${packageName}]: ${version} -> ${latest_version}`);
-        });
-    }
-
-    private getVersion(packageName: string) {
-        const packagePath = require.resolve(`${packageName}/package.json`, {
-            paths: [this.projectDirectory],
-        });
-        const packageJson = require(packagePath);
-        return packageJson.version;
     }
 }
