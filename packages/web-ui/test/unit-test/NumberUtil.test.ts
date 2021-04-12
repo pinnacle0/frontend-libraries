926c05ccaa3bf0ba693b407160edea2ddd22bff9
dionshi <dion.shi.hk@gmail.com>
2021-04-12 14:34:20 +0800

    [FIX]: web-ui: Fix NumberUtil related test cases

---
+++ packages/web-ui/test/unit-test/NumberUtil.test.ts
@@ -0,0 +1,57 @@
+import {NumberUtil} from "../../src/internal/NumberUtil";
+
+describe("NumberUtil.rounding", () => {
+    test("throws if `maxScale` is invalid", () => {
+        expect(() => NumberUtil.rounding(10, "round", -1)).toThrow();
+        expect(() => NumberUtil.rounding(10, "round", 1.1)).toThrow();
+    });
+
+    type TestEachRowSchema = {value: number; algorithm: "round" | "ceil" | "floor"; maxScale: any; expected: number};
+    test.each`
+        value    | algorithm  | maxScale | expected
+        ${3.65}  | ${"floor"} | ${0}     | ${3}
+        ${3.65}  | ${"floor"} | ${1}     | ${3.6}
+        ${3.65}  | ${"floor"} | ${2}     | ${3.65}
+        ${3.65}  | ${"floor"} | ${3}     | ${3.65}
+        ${7.45}  | ${"ceil"}  | ${0}     | ${8}
+        ${7.45}  | ${"ceil"}  | ${1}     | ${7.5}
+        ${7.45}  | ${"ceil"}  | ${2}     | ${7.45}
+        ${7.45}  | ${"ceil"}  | ${3}     | ${7.45}
+        ${1.664} | ${"round"} | ${0}     | ${2}
+        ${1.664} | ${"round"} | ${1}     | ${1.7}
+        ${1.664} | ${"round"} | ${2}     | ${1.66}
+        ${1.664} | ${"round"} | ${3}     | ${1.664}
+        ${1.664} | ${"round"} | ${4}     | ${1.664}
+        ${4.975} | ${"round"} | ${2}     | ${4.98}
+    `("rounding($value, '$algorithm', $maxScale) returns $expected", ({value, algorithm, maxScale, expected}: TestEachRowSchema) => {
+        expect(NumberUtil.rounding(value, algorithm, maxScale)).toBe(expected);
+    });
+});
+
+describe("NumberUtil.roundingToString", () => {
+    test("throws if `scale` is invalid", () => {
+        expect(() => NumberUtil.roundingToString(10, "round", -1)).toThrow();
+        expect(() => NumberUtil.roundingToString(10, "round", 1.1)).toThrow();
+    });
+
+    type TestEachRowSchema = {value: number; algorithm: "round" | "ceil" | "floor"; scale: any; expected: number};
+    test.each`
+        value    | algorithm  | scale | expected
+        ${3.65}  | ${"floor"} | ${0}  | ${"3"}
+        ${3.65}  | ${"floor"} | ${1}  | ${"3.6"}
+        ${3.65}  | ${"floor"} | ${2}  | ${"3.65"}
+        ${3.65}  | ${"floor"} | ${3}  | ${"3.650"}
+        ${7.45}  | ${"ceil"}  | ${0}  | ${"8"}
+        ${7.45}  | ${"ceil"}  | ${1}  | ${"7.5"}
+        ${7.45}  | ${"ceil"}  | ${2}  | ${"7.45"}
+        ${7.45}  | ${"ceil"}  | ${3}  | ${"7.450"}
+        ${1.664} | ${"round"} | ${0}  | ${"2"}
+        ${1.664} | ${"round"} | ${1}  | ${"1.7"}
+        ${1.664} | ${"round"} | ${2}  | ${"1.66"}
+        ${1.664} | ${"round"} | ${3}  | ${"1.664"}
+        ${1.664} | ${"round"} | ${4}  | ${"1.6640"}
+        ${4.975} | ${"round"} | ${2}  | ${"4.98"}
+    `("rounding($value, '$algorithm', $scale) returns '$expected'", ({value, algorithm, scale, expected}: TestEachRowSchema) => {
+        expect(NumberUtil.roundingToString(value, algorithm, scale)).toBe(expected);
+    });
+});
--- packages/web-ui/test/unit-test/TextUtil.test.ts
+++ packages/web-ui/test/unit-test/TextUtil.test.ts
@@ -1,30 +1,5 @@
 import {TextUtil} from "@pinnacle0/web-ui/internal/TextUtil";
 
-describe("TextUtil.numberToFloorFixed", () => {
-    test("floors number with scale", () => {
-        expect(() => TextUtil.numberToFloorFixed(3.5, 1.1)).toThrow();
-        expect(() => TextUtil.numberToFloorFixed(3.5, -2)).toThrow();
-
-        expect(TextUtil.numberToFloorFixed(NaN, 2)).toBe("-");
-        expect(TextUtil.numberToFloorFixed(Infinity, 2)).toBe("-");
-
-        expect(TextUtil.numberToFloorFixed(3.236, 2)).toBe("3.23");
-        expect(TextUtil.numberToFloorFixed(-3.236, 2)).toBe("-3.23");
-        expect(TextUtil.numberToFloorFixed(4324233.236, 1)).toBe("4324233.2");
-        expect(TextUtil.numberToFloorFixed(4324233.236, 0)).toBe("4324233");
-
-        expect(TextUtil.numberToFloorFixed(0, 0)).toBe("0");
-        expect(TextUtil.numberToFloorFixed(0, 1)).toBe("0.0");
-        expect(TextUtil.numberToFloorFixed(0, 2)).toBe("0.00");
-
-        expect(TextUtil.numberToFloorFixed(43.299, 0)).toBe("43");
-        expect(TextUtil.numberToFloorFixed(43.299, 1)).toBe("43.2");
-        expect(TextUtil.numberToFloorFixed(43.299, 2)).toBe("43.29");
-        expect(TextUtil.numberToFloorFixed(43.299, 3)).toBe("43.299");
-        expect(TextUtil.numberToFloorFixed(43.299, 4)).toBe("43.2990");
-    });
-});
-
 describe("TextUtil.interpolate", () => {
     test("interpolates values", () => {
         expect(TextUtil.interpolate("{1} {2}", "hello", "world")).toBe("hello world");
