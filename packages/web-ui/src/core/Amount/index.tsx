9875150e3781fb370fc843600529d80708823730
dionshi <dion.shi.hk@gmail.com>
2021-04-12 14:28:40 +0800

    [REFACTOR]: web-ui: Refactor <Amount> with class-based styles, more flexible to be overwritten

--- packages/web-ui/src/core/Amount/index.tsx
+++ packages/web-ui/src/core/Amount/index.tsx
@@ -1,89 +1,80 @@
 import React from "react";
 import type {PickOptional} from "../../internal/type";
-import {TextUtil} from "../../internal/TextUtil";
+import {NumberUtil} from "../../internal/NumberUtil";
 import {AmountPercentage} from "./AmountPercentage";
+import "./index.less";
 
 export interface Props {
     value: number | null | undefined;
-    scale: number;
-    floorRounding?: boolean;
-    withPlusSignForPositive?: boolean;
-    withThousandSplitter?: boolean;
+    scale: number; // Range [0, 10]
+    rounding?: "floor" | "ceil" | "round";
+    plusSignForPositive?: boolean;
+    thousandSplitter?: boolean;
     nullText?: string;
     prefix?: string;
     postfix?: string;
-    colorScheme?: "green-red" | "highlight" | "highlight-for-positive";
+    colorScheme?: "green+red-" | "highlight" | "highlight+" | "green-red+";
+    bold?: boolean;
     underline?: boolean;
     del?: boolean;
+    className?: string;
+    style?: React.CSSProperties;
 }
 
 export class Amount extends React.PureComponent<Props> {
     static displayName = "Amount";
     static defaultProps: PickOptional<Props> = {
-        withThousandSplitter: true,
+        rounding: "round",
+        thousandSplitter: true,
         nullText: "-",
     };
 
     static Percentage = AmountPercentage;
 
-    private readonly underlineStyle: React.CSSProperties = {textDecoration: "underline", cursor: "pointer"};
-    private readonly greenStyle: React.CSSProperties = {color: "#1abc9b"};
-    private readonly redStyle: React.CSSProperties = {color: "#eb4e36"};
-
-    renderDecimal = (integerPart: string, fractionalPart?: string) => {
-        const {prefix, postfix, del, colorScheme, underline} = this.props;
-        const value = this.props.value as number; // Checked already
-        const amountStyle = underline ? this.underlineStyle : {};
-        let contentNode: React.ReactElement = fractionalPart ? (
-            <React.Fragment>
-                {integerPart}.<small>{fractionalPart}</small>
-            </React.Fragment>
-        ) : (
-            <React.Fragment>{integerPart}</React.Fragment>
-        );
-
-        contentNode = (
-            <span style={amountStyle}>
-                {prefix ? prefix + " " : null}
-                {contentNode}
-                {postfix ? " " + postfix : null}
-            </span>
-        );
-
-        if (colorScheme) {
-            if (colorScheme === "green-red") {
-                const coloredStyle: React.CSSProperties = {
-                    ...amountStyle,
-                    ...(value > 0 ? this.greenStyle : {}),
-                    ...(value < 0 ? this.redStyle : {}),
-                };
-                contentNode = <span style={coloredStyle}>{contentNode}</span>;
-            } else if (colorScheme === "highlight-for-positive") {
-                contentNode = value > 0 ? <em>{contentNode}</em> : contentNode;
-            } else if (colorScheme === "highlight") {
-                contentNode = <em>{contentNode}</em>;
-            }
-        }
-
-        if (del) {
-            contentNode = <del>{contentNode}</del>;
-        }
-
-        return contentNode;
-    };
-
     render() {
-        const {value, scale, floorRounding, withThousandSplitter, withPlusSignForPositive, nullText} = this.props;
+        const {value, scale, rounding, thousandSplitter, plusSignForPositive, prefix, postfix, colorScheme, bold, del, underline, style, className, nullText} = this.props;
         if (value !== null && value !== undefined && Number.isFinite(value)) {
-            const rounded = floorRounding ? TextUtil.numberToFloorFixed(value, scale) : value.toFixed(scale);
-            const parts = rounded.split(".");
-            if (withThousandSplitter) {
+            const roundedString = NumberUtil.roundingToString(value, rounding!, scale);
+            const parts = roundedString.split(".");
+            if (thousandSplitter) {
                 parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
             }
-            if (withPlusSignForPositive && value > 0) {
+            if (plusSignForPositive && value > 0) {
                 parts[0] = "+" + parts[0];
             }
-            return this.renderDecimal(parts[0], parts[1]);
+
+            const classNames = ["g-amount"];
+            if (bold) classNames.push("bold");
+            if (del) classNames.push("del");
+            if (underline) classNames.push("underline");
+            if (className) classNames.push(className);
+
+            if (colorScheme === "green+red-") {
+                if (value > 0) classNames.push("color-green");
+                if (value < 0) classNames.push("color-red");
+            } else if (colorScheme === "green-red+") {
+                if (value > 0) classNames.push("color-red");
+                if (value < 0) classNames.push("color-green");
+            } else if (colorScheme === "highlight") {
+                classNames.push("color-highlight");
+            } else if (colorScheme === "highlight+") {
+                if (value > 0) classNames.push("color-highlight");
+            }
+
+            return (
+                <div className={classNames.join(" ")} style={style}>
+                    {prefix && <div className="prefix">{prefix}</div>}
+                    <div className="digits">
+                        {parts[0]}
+                        {parts[1] && (
+                            <React.Fragment>
+                                .<small>{parts[1]}</small>
+                            </React.Fragment>
+                        )}
+                    </div>
+                    {postfix && <div className="postfix">{postfix}</div>}
+                </div>
+            );
         } else {
             return nullText;
         }
--- stylelint.config.js
+++ stylelint.config.js
@@ -4,7 +4,7 @@
 module.exports = {
     extends: ["stylelint-config-standard", "stylelint-config-prettier"],
     rules: {
-        // "no-descending-specificity": null,
+        "no-descending-specificity": null,
         "rule-empty-line-before": ["always", {except: ["first-nested", "after-single-line-comment"]}],
     },
 };
