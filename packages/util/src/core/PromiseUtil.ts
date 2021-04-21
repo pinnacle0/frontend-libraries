d5078a3339eeeadaeafe6c72f8e6b2b6b5070370
yuen <yuencheung0@gmail.com>
2021-04-21 16:48:46 +0800

    [REFACTOR]: util: add raceSuccess

--- packages/util/src/core/PromiseUtil.ts
+++ packages/util/src/core/PromiseUtil.ts
@@ -6,7 +6,26 @@ function sleepThenResolve<T>(ms: number, resolvedValue: T): Promise<T> {
     return new Promise<T>(resolve => setTimeout(() => resolve(resolvedValue), ms));
 }
 
+/**
+ * Only resolve with the first-resolved value.
+ * If none of the promises resolves (all rejects), this function rejects with a list of errors (rejected values).
+ * Ref: https://stackoverflow.com/questions/37234191/resolve-es6-promise-with-first-success
+ */
+function raceSuccess<T>(promises: Array<PromiseLike<T>>): PromiseLike<T> {
+    const reversedPromises = promises.map(_ =>
+        _.then(
+            val => Promise.reject(val),
+            err => Promise.resolve(err)
+        )
+    );
+    return Promise.all(reversedPromises).then(
+        errors => Promise.reject(errors),
+        val => Promise.resolve(val)
+    );
+}
+
 export const PromiseUtil = Object.freeze({
     sleep,
     sleepThenResolve,
+    raceSuccess,
 });
---
+++ packages/util/test/core/PromiseUtil.test.ts
@@ -0,0 +1,28 @@
+import {PromiseUtil} from "../../src/core/PromiseUtil";
+
+const sleep = <T>(ms: number, resolvedValue: T) => new Promise<T>(resolve => setTimeout(() => resolve(resolvedValue), ms));
+const sleepThenReject = <T>(ms: number, rejectedValue: T) => new Promise<T>((resolve, reject) => setTimeout(() => reject(rejectedValue), ms));
+
+/**
+ * Return statement is required for promise-based tests.
+ * Ref: https://jestjs.io/docs/en/asynchronous.html#resolves-rejects
+ */
+
+test("Promises Race Success", () => {
+    const promises: Array<Promise<string>> = [
+        sleep(500, "a"),
+        sleep(400, "fastest"),
+        sleep(800, "b"),
+        sleep(10000, "c"), // No matter how long it is, no waiting here
+        Promise.reject("error"),
+        sleepThenReject(300, "error-300"),
+    ];
+
+    return expect(PromiseUtil.raceSuccess(promises)).resolves.toBe("fastest");
+});
+
+test("Promises Race Success (All Errors)", () => {
+    const promises: Array<Promise<string>> = [sleepThenReject(300, "error-300"), sleepThenReject(500, "error-500")];
+
+    return expect(PromiseUtil.raceSuccess(promises)).rejects.toHaveLength(promises.length);
+});
