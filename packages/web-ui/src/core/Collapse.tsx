f6de5da54a3350a3cfff07d28da68c45217a8c04
Alvis Tse <alvistse123@gmail.com>
2021-07-12 11:04:20 +0800

    [REFACTOR]: web-ui: Added Panel to Collapse component

--- packages/web-ui/src/core/Collapse.tsx
+++ packages/web-ui/src/core/Collapse.tsx
@@ -3,10 +3,13 @@ import AntdCollapse from "antd/lib/collapse";
 import type {CollapseProps, CollapsePanelProps} from "antd/lib/collapse";
 import "antd/lib/collapse/style";
 
+/** Will be remove in future, replaced by Foldable and Accordion */
 export interface Props extends CollapseProps {}
 
 export class Collapse extends React.PureComponent<Props> {
     static displayName = "Collapse";
+
+    static Panel = AntdCollapse.Panel;
 
     render() {
         return <AntdCollapse {...this.props} />;
