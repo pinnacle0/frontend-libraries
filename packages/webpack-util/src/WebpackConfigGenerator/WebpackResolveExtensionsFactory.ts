38ce58cd5385a196dce01ac2d2fbfecf857c3516
dionshi <dion.shi.hk@gmail.com>
2021-07-28 16:32:32 +0800

    [REFACTOR]: webpack-util: 0.2.4: Upgrade deps (mainly style-loder/css-loader) and simplify resolve.extension factory usage

--- packages/webpack-util/src/WebpackConfigGenerator/WebpackResolveExtensionsFactory.ts
+++ packages/webpack-util/src/WebpackConfigGenerator/WebpackResolveExtensionsFactory.ts
@@ -9,16 +9,7 @@ export class WebpackResolveExtensionsFactory {
         const resolveExtensions = [];
 
         for (const prefix of prioritizedExtensionPrefixes) {
-            const extensions = [
-                `.${prefix}.ts`,
-                `.${prefix}.tsx`,
-                `.${prefix}.js`,
-                `.${prefix}.jsx`,
-                `.${prefix}.less`,
-                `.${prefix}.css`,
-                // prettier-format-preserve
-            ];
-            resolveExtensions.push(...extensions);
+            resolveExtensions.push(...Constant.resolveExtensions.map(_ => `.${prefix}${_}`));
         }
 
         resolveExtensions.push(...Constant.resolveExtensions);
