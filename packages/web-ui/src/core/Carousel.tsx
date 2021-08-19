6a14d3900699ba0525f17775f6556ab72da4bb9f
Jamyth Luk <jamythluk@jamyth.local>
2021-08-19 11:54:46 +0800

    [REFACTOR]: web-ui: tune Carousel Ref usage

--- packages/web-ui/src/core/Carousel.tsx
+++ packages/web-ui/src/core/Carousel.tsx
@@ -1,14 +1,19 @@
-import type {CarouselProps as AntCarouselProps} from "antd/lib/carousel";
+import type {CarouselProps as AntCarouselProps, CarouselRef} from "antd/lib/carousel";
 import AntCarousel from "antd/lib/carousel";
 import React from "react";
 import "antd/lib/carousel/style";
 
-export interface Props extends AntCarouselProps {}
+export type {CarouselRef} from "antd/lib/carousel";
+
+export interface Props extends AntCarouselProps {
+    innerRef?: React.Ref<CarouselRef>;
+}
 
 export class Carousel extends React.PureComponent<Props> {
     static displayName = "Carousel";
 
     render() {
-        return <AntCarousel {...this.props} />;
+        const {innerRef, ...props} = this.props;
+        return <AntCarousel ref={innerRef} {...props} />;
     }
 }
