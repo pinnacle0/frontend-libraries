771642dc09a3518216f57853ce27641569595971
Alvis Tse <alvistse123@gmail.com>
2021-10-05 15:38:45 +0800

    [REFACTOR]: eslint-plugin: codegen config and reules

--- packages/eslint-plugin/src/rules/index.ts
+++ packages/eslint-plugin/src/rules/index.ts
@@ -5,38 +5,38 @@
 // -----------------------------------------------------------------------------
 
 import type {RuleListener, RuleModule} from "@typescript-eslint/experimental-utils/dist/ts-eslint";
+import {rule as explicitModuleClassMethodReturnType} from "./explicit-module-class-method-return-type";
+import {rule as importOrdering} from "./import-ordering";
 import {rule as moduleClassLifecycleOrder} from "./module-class-lifecycle-order";
 import {rule as moduleClassMethodDecorators} from "./module-class-method-decorators";
+import {rule as noDeepNestedRelativeImports} from "./no-deep-nested-relative-imports";
+import {rule as noNamedImports} from "./no-named-imports";
+import {rule as noUglyRelativePath} from "./no-ugly-relative-path";
+import {rule as noUnnecessaryEndingIndex} from "./no-unnecessary-ending-index";
 import {rule as reactComponentDefaultPropsTyping} from "./react-component-default-props-typing";
 import {rule as reactComponentDisplayName} from "./react-component-display-name";
 import {rule as reactComponentEventHandlerNaming} from "./react-component-event-handler-naming";
 import {rule as reactComponentMethodOrdering} from "./react-component-method-ordering";
 import {rule as reactComponentPropsTyping} from "./react-component-props-typing";
 import {rule as reactComponentStateMember} from "./react-component-state-member";
-import {rule as noNamedImports} from "./no-named-imports";
-import {rule as noDeepNestedRelativeImports} from "./no-deep-nested-relative-imports";
-import {rule as noUglyRelativePath} from "./no-ugly-relative-path";
-import {rule as noUnnecessaryEndingIndex} from "./no-unnecessary-ending-index";
 import {rule as stylePropertiesType} from "./style-properties-type";
-import {rule as importOrdering} from "./import-ordering";
-import {rule as explicitModuleClassMethodReturnType} from "./explicit-module-class-method-return-type";
 import {rule as variableDeclarationModuleIdentifierShadowing} from "./variable-declaration-module-identifier-shadowing";
 
 export const rules: Record<string, RuleModule<string, any[], RuleListener>> = {
+    "explicit-module-class-method-return-type": explicitModuleClassMethodReturnType,
+    "import-ordering": importOrdering,
     "module-class-lifecycle-order": moduleClassLifecycleOrder,
     "module-class-method-decorators": moduleClassMethodDecorators,
-    "import-ordering": importOrdering,
+    "no-deep-nested-relative-imports": noDeepNestedRelativeImports,
+    "no-named-imports": noNamedImports,
+    "no-ugly-relative-path": noUglyRelativePath,
+    "no-unnecessary-ending-index": noUnnecessaryEndingIndex,
     "react-component-default-props-typing": reactComponentDefaultPropsTyping,
     "react-component-display-name": reactComponentDisplayName,
     "react-component-event-handler-naming": reactComponentEventHandlerNaming,
     "react-component-method-ordering": reactComponentMethodOrdering,
     "react-component-props-typing": reactComponentPropsTyping,
     "react-component-state-member": reactComponentStateMember,
-    "no-ugly-relative-path": noUglyRelativePath,
-    "no-unnecessary-ending-index": noUnnecessaryEndingIndex,
-    "no-named-imports": noNamedImports,
-    "no-deep-nested-relative-imports": noDeepNestedRelativeImports,
-    "explicit-module-class-method-return-type": explicitModuleClassMethodReturnType,
     "style-properties-type": stylePropertiesType,
     "variable-declaration-module-identifier-shadowing": variableDeclarationModuleIdentifierShadowing,
 };
