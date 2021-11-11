9376188d442766c7d353b5ffe73fbeb7c925982e
Alvis Tse <alvistse123@gmail.com>
2021-11-11 16:28:02 +0800

    [FIX]: Fixed stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,10 +1,14 @@
 // @ts-check
 
-/** @type {import("stylelint").Configuration} */
+/** @type {import("stylelint").Config} */
 module.exports = {
+    customSyntax: "postcss-less",
     extends: ["stylelint-config-standard", "stylelint-config-prettier"],
     rules: {
         "no-descending-specificity": null,
         "rule-empty-line-before": ["always", {except: ["first-nested", "after-single-line-comment"]}],
+        "no-invalid-position-at-import-rule": null,
+        "declaration-block-no-redundant-longhand-properties": null,
+        "property-no-vendor-prefix": null,
     },
 };
