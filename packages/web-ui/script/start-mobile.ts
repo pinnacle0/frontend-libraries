60abc51d8e137bae511bd4e421ed2eb92b283dfd
Alvis Tse <alvistse123@gmail.com>
2022-03-15 10:30:44 +0800

    [REFACTOR]: web-ui: setup test env for mobile ui

---
+++ packages/web-ui/script/start-mobile.ts
@@ -0,0 +1,13 @@
+import path from "path";
+import {WebpackUtil} from "@pinnacle0/webpack-util/src";
+
+new WebpackUtil.ServerStarter({
+    projectDirectory: path.resolve(__dirname, "../test/mobile-ui-test"),
+    port: 4456,
+    dynamicPathResolvers: [
+        {
+            prefix: "@pinnacle0/web-ui",
+            resolver: path.resolve(__dirname, "../src"),
+        },
+    ],
+}).run();
