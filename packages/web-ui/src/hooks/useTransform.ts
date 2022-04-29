2170c84632948397c6ab270b4e634d58a0e482f7
Alvis Tse <alvistse123@gmail.com>
2022-04-29 11:53:42 +0800

    [REFACTOR]: web-ui: hooks/useTransform Support scale

--- packages/web-ui/src/hooks/useTransform.ts
+++ packages/web-ui/src/hooks/useTransform.ts
@@ -1,27 +1,59 @@
 import React from "react";
 
-interface TransformOption {
+interface TransitionOption {
+    // transition related properties
+    property?: string;
+    // in ms
+    duration?: number;
+    delay?: number;
+    timingFunction?: string;
+}
+
+interface Option extends TransitionOption {
+    // transform related properties
     x?: number;
     y?: number;
     z?: number;
     immediate?: boolean;
 }
 
-export const useTransform = (ref: React.RefObject<HTMLElement>, initialOption?: TransformOption) => {
-    const initialOptionRef = React.useRef(initialOption);
-    initialOptionRef.current = initialOption;
+export const useTransform = (ref: React.RefObject<HTMLElement>, option?: Option) => {
+    const optionRef = React.useRef(option);
+    optionRef.current = option;
+
+    const clearTransition = React.useCallback((el: HTMLElement) => {
+        el.style.transitionDuration = "";
+        el.style.transitionProperty = "";
+        el.style.transitionTimingFunction = "";
+        el.style.transitionDuration = "";
+    }, []);
+
+    const setTransition = React.useCallback(
+        (el: HTMLElement) => {
+            if (optionRef.current) {
+                const {delay, duration, property, timingFunction} = optionRef.current;
+                delay && (el.style.transitionDelay = delay + "ms");
+                duration && (el.style.transitionDuration = duration + "ms");
+                property && (el.style.transitionProperty = property);
+                timingFunction && (el.style.transitionTimingFunction = timingFunction);
+            } else {
+                clearTransition(el);
+            }
+        },
+        [clearTransition]
+    );
 
     const to = React.useCallback(
-        (option: TransformOption) => {
+        (option: Option) => {
             if (ref.current) {
                 const el = ref.current;
                 requestAnimationFrame(() => {
                     el.style.transform = `translate3d(${option.x ?? 0}px, ${option.y ?? 0}px, ${option.z ?? 0}px) `;
-                    option.immediate ? (el.style.transition = "none") : (el.style.transition = "");
+                    option.immediate ? clearTransition(el) : setTransition(el);
                 });
             }
         },
-        [ref]
+        [ref, setTransition, clearTransition]
     );
 
     const clear = React.useCallback(() => {
@@ -29,15 +61,14 @@ export const useTransform = (ref: React.RefObject<HTMLElement>, initialOption?: 
             const el = ref.current;
             requestAnimationFrame(() => {
                 el.style.transform = "";
-                el.style.transition = "";
+                setTransition(el);
             });
         }
-    }, [ref]);
+    }, [ref, setTransition]);
 
-    // TODO/Alvis: this can be wrong in fast refresh mode, need to improve
     React.useEffect(() => {
-        if (initialOptionRef.current) {
-            to(initialOptionRef.current);
+        if (optionRef.current) {
+            to(optionRef.current);
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps -- only run on mount
     }, []);
