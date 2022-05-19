65661e9d4f565078b942622a995b0ec42d140b11
dionshi <dion.shi.hk@gmail.com>
2022-05-19 11:31:59 +0800

    [REFACTOR]: webpack-util: Tune script-tag-crossorigin-plugin

--- packages/webpack-util/src/WebpackConfigGenerator/Plugin/script-tag-crossorigin-plugin.ts
+++ packages/webpack-util/src/WebpackConfigGenerator/Plugin/script-tag-crossorigin-plugin.ts
@@ -3,8 +3,8 @@ import type webpack from "webpack";
 
 const PLUGIN_NAME = "ScriptTagCrossOriginPlugin";
 
-// This plugin add crossorigin="anonymous" to html-webpack-plugin generated js script tag, only work with html-webpack-plugin@4.0.0 and above
-export class ScriptTagCrossOriginPlugin {
+// This plugin adds crossorigin="anonymous" to html-webpack-plugin generated <script> tags, only work with html-webpack-plugin@4.0.0+
+export class ScriptTagCrossOriginPlugin implements webpack.WebpackPluginInstance {
     apply(compiler: webpack.Compiler): void {
         compiler.hooks.compilation.tap(PLUGIN_NAME, compilation => {
             const hook = HtmlWebpackPlugin.getHooks(compilation).alterAssetTags;
