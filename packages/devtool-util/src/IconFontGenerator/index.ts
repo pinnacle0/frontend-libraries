390a8c58c630d7cc85b1ebc81d220d583f7aa916
dionshi <dion.shi.hk@gmail.com>
2022-07-13 15:38:11 +0800

    [REFACTOR]: devtool-util: Remove unused legacy IconFontGenerator

--- packages/devtool-util/src/IconFontGenerator/index.ts
+++
@@ -1,7 +0,0 @@
-import {AppIconFontGenerator} from "./AppIconFontGenerator";
-import {WebIconFontGenerator} from "./WebIconFontGenerator";
-
-export const IconFontGenerator = Object.freeze({
-    Web: WebIconFontGenerator,
-    App: AppIconFontGenerator,
-});
