1c55c376cc591fd4fb9d1ac4d35cccbb9375cdcd
uay13211 <david19961113@gmail.com>
2022-09-09 10:31:06 +0800

    [REFACTOR]: web-ui: add Image component

---
+++ packages/web-ui/src/core/Image.tsx
@@ -0,0 +1,14 @@
+import type {ImageProps} from "antd/lib/image";
+import AntImage from "antd/lib/image";
+import React from "react";
+import "antd/lib/card/style";
+
+export interface Props extends ImageProps {}
+
+export class Image extends React.PureComponent<Props> {
+    static displayName = "Image";
+
+    render() {
+        return <AntImage {...this.props} />;
+    }
+}
