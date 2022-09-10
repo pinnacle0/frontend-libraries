004f04d14d175eaf240e5a71e4973a61783b2743
Alvis Tse <alvistse123@gmail.com>
2022-09-10 19:48:57 +0800

    [CHORE]: all: updated pnpm-single-version, added perpare script, fix eslint version

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,12 +1,7 @@
+const hook = require("./.psv/hook");
+
 module.exports = {
     hooks: {
-        afterAllResolved: (() => {
-            try {
-                require.resolve("pnpm-single-version");
-                return require("pnpm-single-version").checkSingleVersion;
-            } catch {
-                return;
-            }
-        })(),
+        afterAllResolved: hook,
     },
 };
