5e18c6aa891ff51c5b5be7a7fa690a3884a45c8f
Alvis Tse <alvistse123@gmail.com>
2022-10-05 20:37:54 +0800

    [CHORE]: codemod: updated comment

--- packages/codemod/test/createTest.ts
+++ packages/codemod/test/createTest.ts
@@ -3,6 +3,10 @@ import {defineTest, defineInlineTest} from "jscodeshift/dist/testUtils";
 import path from "path";
 import type {ModType} from "../src/modType";
 
+/**
+ * Test fixtures is located in test/__testfixtures__/<codemod-name>
+ * A test should be provided with both <prefix>.input.tsx and <prefix>.output.tsx
+ */
 export const createTest = function (type: ModType, prefix: string) {
     defineTest(path.join(__dirname, "test"), `../src/mod/${type}`, null, `${type}/${prefix}`, {parser: "tsx"});
 };
