6afbeb65b599073aeff1102aa3414e9cd48ba8da
uay13211 <david19961113@gmail.com>
2022-11-03 16:08:26 +0800

    [REFACTOR]: web-ui: add ResizeObserverPolyfill

---
+++ packages/web-ui/src/core/ResizeObserverPolyfill.ts
@@ -0,0 +1,9 @@
+(async () => {
+    if ("ResizeObserver" in window === false) {
+        // Loads polyfill asynchronously, only if required.
+        const module = await import("@juggle/resize-observer");
+        window.ResizeObserver = module.ResizeObserver;
+    }
+})();
+
+export {};
