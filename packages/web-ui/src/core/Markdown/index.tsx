ed23a072ffd2977a9211d8435247ef84ab2db73f
dionshi <dion.shi.hk@gmail.com>
2022-11-15 15:03:59 +0800

    [REFACTOR]: web-ui: Minor tune Markdown

--- packages/web-ui/src/core/Markdown/index.tsx
+++ packages/web-ui/src/core/Markdown/index.tsx
@@ -3,9 +3,9 @@ import "./index.less";
 
 /**
  * Currently, <Markdown> supports:
- * - \n for new line, each line wrapped with <p/>.
- * - `text` for emphasis, transform to <em>text</em>.
- * - __text__ for boldness, transform to <b>text</b>.
+ * - \n for new line, each line wrapped with <div class="line">
+ * - `text` for emphasis, transform to <em>
+ * - __text__ or **text** for boldness, transform to <b>
  *
  * Nested usage not supported yet.
  */
@@ -20,6 +20,33 @@ export interface Props {
 export class Markdown extends React.PureComponent<Props> {
     static displayName = "Markdown";
 
+    renderBold = (splitContents: string[], symbols: MarkdownSymbol[]) => {
+        return splitContents.map((_, segmentIndex) => {
+            const text = this.processLine(_, segmentIndex, symbols.slice(1));
+            return segmentIndex % 2 === 1 ? <b key={`bold${segmentIndex}`}>{text}</b> : text;
+        });
+    };
+
+    renderEmphasis = (splitContents: string[], symbols: MarkdownSymbol[]) => {
+        return splitContents.map((_, segmentIndex) => {
+            const text = this.processLine(_, segmentIndex, symbols.slice(1));
+            return segmentIndex % 2 === 1 ? <em key={`emphasis${segmentIndex}`}>{text}</em> : text;
+        });
+    };
+
+    processLine = (content: string, index: number, symbols: MarkdownSymbol[]): React.ReactNode => {
+        switch (symbols[0]) {
+            case "**":
+                return this.renderBold(content.split(/\*\*/g), symbols);
+            case "__":
+                return this.renderBold(content.split(/__/g), symbols);
+            case "`":
+                return this.renderEmphasis(content.split(/`/g), symbols);
+            default:
+                return content;
+        }
+    };
+
     render() {
         const {children, style, whitelist} = this.props;
         const symbols = whitelist || ["**", "__", "`"];
@@ -33,32 +60,5 @@ export class Markdown extends React.PureComponent<Props> {
                 ))}
             </div>
         );
-    }
-
-    private processLine = (content: string, index: number, symbols: MarkdownSymbol[]): React.ReactNode[] => {
-        switch (symbols[0]) {
-            case "**":
-                return this.renderBold(content.split(/\*\*/g), symbols);
-            case "__":
-                return this.renderBold(content.split(/__/g), symbols);
-            case "`":
-                return this.renderEmphasis(content.split(/`/g), symbols);
-            default:
-                return [<React.Fragment key={index}>{content}</React.Fragment>];
-        }
-    };
-
-    private renderBold(splitContents: string[], symbols: MarkdownSymbol[]) {
-        return splitContents.map((_, segmentIndex) => {
-            const text = this.processLine(_, segmentIndex, symbols.slice(1));
-            return segmentIndex % 2 === 1 ? <b key={`bold${segmentIndex}`}>{text}</b> : text;
-        });
-    }
-
-    private renderEmphasis(splitContents: string[], symbols: MarkdownSymbol[]) {
-        return splitContents.map((_, segmentIndex) => {
-            const text = this.processLine(_, segmentIndex, symbols.slice(1));
-            return segmentIndex % 2 === 1 ? <em key={`emphasis${segmentIndex}`}>{text}</em> : text;
-        });
     }
 }
