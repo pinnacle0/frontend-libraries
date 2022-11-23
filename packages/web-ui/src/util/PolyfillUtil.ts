1c655289cb5e595e158fe867da3864fc2c2a2a6a
uay13211 <david19961113@gmail.com>
2022-11-23 12:04:16 +0800

    [REFACTOR]: web-ui: format polyfillUtil

--- packages/web-ui/src/util/PolyfillUtil.ts
+++ packages/web-ui/src/util/PolyfillUtil.ts
@@ -1,9 +1,9 @@
-import {ResizeObserver} from "@juggle/resize-observer"
+import {ResizeObserver} from "@juggle/resize-observer";
 
 export class PolyfillUtil {
     static ResizeObserver() {
         if ("ResizeObserver" in window === false) {
-            window.ResizeObserver = ResizeObserver
+            window.ResizeObserver = ResizeObserver;
         }
     }
 }
