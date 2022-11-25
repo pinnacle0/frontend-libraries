291c4d90057946086e61e8386322f2a7145dcc44
dionshi <dion.shi.hk@gmail.com>
2022-11-25 11:37:34 +0800

    [REFACTOR]: webpack-util: 0.3.42 force other rule to emit file instead of auto-detect

--- packages/webpack-util/src/WebpackConfigGenerator/Rule/other.rule.ts
+++ packages/webpack-util/src/WebpackConfigGenerator/Rule/other.rule.ts
@@ -14,8 +14,9 @@ interface OtherRuleDeps {
  */
 export function otherRule({extraExtensionsForOtherRule}: OtherRuleDeps): webpack.RuleSetRule {
     return {
+        // put `ico` here instead of image.rule, to avoid inline favicon into HTML
         test: RegExpUtil.fileExtension(".ico", ...Constant.mediaExtensions, ...Constant.fontExtensions, ...extraExtensionsForOtherRule),
-        type: "asset",
+        type: "asset/resource",
         generator: {
             filename: "static/other/[name].[hash:8][ext][query]",
         },
