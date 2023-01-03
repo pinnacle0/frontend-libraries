1f8c76630b97cf39bd80553e4fef9ecee2995ab6
Alvis Tse <alvistse123@gmail.com>
2023-01-03 10:54:53 +0800

    [FEATURE]: react-stack-router: component/Link add state, replace props

--- packages/react-stack-router/src/component/Link.tsx
+++ packages/react-stack-router/src/component/Link.tsx
@@ -3,16 +3,18 @@ import {createPath} from "history";
 import {RouterContext} from "../context";
 import type {To} from "history";
 
-interface Props extends Omit<React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>, "href" | "ref"> {
+interface Props extends Omit<React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>, "href"> {
     to: To;
+    replace?: boolean;
+    state?: any;
 }
 
-export const Link = forwardRef<HTMLAnchorElement, Props>(({to, children, target, onClick, ...restProps}, ref) => {
-    const {push} = useContext(RouterContext);
+export const Link = forwardRef<HTMLAnchorElement, Props>(({to, children, target, onClick, replace: isReplace, state, ...restProps}, ref) => {
+    const {push, replace} = useContext(RouterContext);
     const herf = useMemo(() => (typeof to === "string" ? to : createPath(to)), [to]);
     const handleClick: React.MouseEventHandler<HTMLAnchorElement> = event => {
         event.preventDefault();
-        push(to);
+        isReplace ? replace(to, state) : push(to, state);
         onClick?.(event);
     };
 
