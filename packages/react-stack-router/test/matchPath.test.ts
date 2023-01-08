375545b45499fa5e83032bb8c97fa028d22e03c7
Alvis Tse <alvistse123@gmail.com>
2023-01-08 14:05:49 +0800

    [REFACTOR]: react-stack-router: rename matchPath test

---
+++ packages/react-stack-router/test/matchPath.test.ts
@@ -0,0 +1,39 @@
+import {matchPathSegment, patternType} from "../src/route/matchPath";
+describe("match path segment testing", () => {
+    test.each`
+        path             | expected
+        ${":id"}         | ${"parameter"}
+        ${":tab(A|B|C)"} | ${"union"}
+        ${":tab(|A|||)"} | ${"union"}
+        ${":tab(A)"}     | ${"union"}
+        ${":**"}         | ${"parameter"}
+        ${"*"}           | ${"wildcard"}
+        ${"("}           | ${"normal"}
+        ${"#sdfsdfdsf"}  | ${"normal"}
+        ${"**"}          | ${"any"}
+        ${"***"}         | ${"normal"}
+    `("patternType of $path should be '$expected'", ({path, expected}) => {
+        expect(patternType(path)).toBe(expected);
+    });
+
+    test.each`
+        pattern                   | path         | result                          | throwError
+        ${":id"}                  | ${"123"}     | ${{param: {id: "123"}}}         | ${false}
+        ${"/:id"}                 | ${"123"}     | ${null}                         | ${false}
+        ${":id(123|456)"}         | ${"456"}     | ${{param: {id: "456"}}}         | ${false}
+        ${":id(123|456)"}         | ${"bbc"}     | ${null}                         | ${false}
+        ${":id(456)"}             | ${"456"}     | ${{param: {id: "456"}}}         | ${false}
+        ${":id(456)"}             | ${"123"}     | ${null}                         | ${false}
+        ${":id(123_456|456_789)"} | ${"456_789"} | ${{param: {id: "456_789"}}}     | ${false}
+        ${":id(123_456|456+789)"} | ${"456+789"} | ${null}                         | ${false}
+        ${":id())"}               | ${"456"}     | ${null}                         | ${true}
+        ${"*"}                    | ${"456"}     | ${{param: {}, wildcard: "456"}} | ${false}
+        ${":*"}                   | ${"abc"}     | ${{param: {"*": "abc"}}}        | ${false}
+    `("matchPath($path, $pattern) should be $result", ({path, pattern, result, throwError}) => {
+        if (!throwError) {
+            expect(matchPathSegment(pattern, path)).toStrictEqual(result);
+        } else {
+            expect(() => matchPathSegment(pattern, path)).toThrowError();
+        }
+    });
+});
