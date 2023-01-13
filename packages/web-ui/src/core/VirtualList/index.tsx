5cadefdeb5333d0950357213f4727b7a7320c2f5
Alvis Tse <alvistse123@gmail.com>
2023-01-13 19:19:10 +0800

    [REFACTOR]: web-ui: update VirtualList, added index.less

--- packages/web-ui/src/core/VirtualList/index.tsx
+++ packages/web-ui/src/core/VirtualList/index.tsx
@@ -1,13 +1,17 @@
-import "../internal/polyfill/ResizeObserver";
+import "../../internal/polyfill/ResizeObserver";
 import React from "react";
+import {classNames} from "../../util/ClassNames";
 import {useVirtualizer, observeElementOffset as defaultObserveElementOffset, observeElementRect as defaultObserveElementRect} from "@tanstack/react-virtual";
-import type {VirtualizerOptions} from "@tanstack/react-virtual";
+import type {VirtualizerOptions, Virtualizer} from "@tanstack/react-virtual";
 import type {ComponentType} from "react";
-import type {StringKey} from "../internal/type";
+import type {StringKey} from "../../internal/type";
+import "./index.less";
 
 const DEFAULT_ITEM_SIZE = 100;
 
-type Direction = "horizontal" | "vertical";
+export type Direction = "horizontal" | "vertical";
+
+export interface VirtualListHandler extends Pick<Virtualizer<HTMLElement, HTMLElement>, "scrollElement" | "scrollToIndex" | "scrollToOffset" | "measure" | "getVirtualItems"> {}
 
 export interface ItemProps<T extends object> {
     index: number;
@@ -24,6 +28,8 @@ export interface Props<T extends object> {
     overscan?: number;
     observeElementRect?: VirtualizerOptions<HTMLElement, HTMLElement>["observeElementRect"];
     observeElementOffset?: VirtualizerOptions<HTMLElement, HTMLElement>["observeElementOffset"];
+    className?: string;
+    listRef?: React.Ref<VirtualListHandler>;
 }
 
 /**
@@ -39,11 +45,12 @@ export function VirtualList<T extends object>({
     observeElementOffset,
     fixedSize,
     observeElementRect,
+    className,
+    listRef,
 }: Props<T>) {
     const Item = renderData;
     const parentRef = React.useRef<HTMLDivElement | null>(null);
     const horizontal = direction === "horizontal";
-    const sizeStyle: React.CSSProperties = horizontal ? {height: "100%"} : {width: "100%"};
     const virtualizer = useVirtualizer<HTMLElement, HTMLElement>({
         initialRect,
         horizontal,
@@ -55,33 +62,28 @@ export function VirtualList<T extends object>({
         overscan,
     });
 
+    React.useImperativeHandle(listRef, () => ({
+        getVirtualItems: virtualizer.getVirtualItems,
+        scrollElement: virtualizer.scrollElement,
+        measure: virtualizer.measure,
+        scrollToIndex: virtualizer.scrollToIndex,
+        scrollToOffset: virtualizer.scrollToOffset,
+    }));
+
     const getItemKey = (index: number) => (rowKey === "index" ? index : data[index][rowKey]);
 
     return (
-        <div className="g-virtual-list" ref={parentRef} style={{flex: 1, width: "100%", height: "100%", overflow: "auto"}}>
-            <div
-                className="g-virtual-list-inner"
-                style={{
-                    position: "relative",
-                    height: horizontal ? "100%" : virtualizer.getTotalSize(),
-                    width: horizontal ? virtualizer.getTotalSize() : "100%",
-                }}
-            >
+        <div className={classNames("g-virtual-list", className)} ref={parentRef}>
+            <div className={classNames("g-virtual-list-inner", {horizontal})} style={horizontal ? {width: virtualizer.getTotalSize()} : {height: virtualizer.getTotalSize()}}>
                 {virtualizer.getVirtualItems().map(virtualRow => (
                     <div
-                        className="g-virtual-list-item"
+                        className={classNames("g-virtual-list-item", {horizontal})}
                         key={getItemKey(virtualRow.index) as string | number}
                         ref={virtualizer.measureElement}
                         data-index={virtualRow.index}
-                        style={{
-                            position: "absolute",
-                            top: 0,
-                            left: 0,
-                            transform: horizontal ? `translateX(${virtualRow.start}px)` : `translateY(${virtualRow.start}px)`,
-                            ...sizeStyle,
-                        }}
+                        style={{transform: horizontal ? `translateX(${virtualRow.start}px)` : `translateY(${virtualRow.start}px)`}}
                     >
-                        <div className="g-virtual-list-item-wrapper" style={{...sizeStyle}}>
+                        <div className={classNames("g-virtual-list-item-wrapper", {horizontal})}>
                             <Item data={data[virtualRow.index]} index={virtualRow.index} />
                         </div>
                     </div>
