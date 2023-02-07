44a49cb56ae4ff7abf9ab3ff6856186536e648ef
Alvis Tse <alvistse123@gmail.com>
2023-02-07 15:34:18 +0800

    [FIX]: web-ui: fixed useDebounce closure issue in callback

--- packages/web-ui/src/hooks/useDebounce.ts
+++ packages/web-ui/src/hooks/useDebounce.ts
@@ -1,18 +1,21 @@
 import React from "react";
 
-export const useDebounce = function <Param extends any[]>(callback: (...args: Param) => void) {
-    const idRef = React.useRef<number>();
+type Callback<T extends any[]> = (...args: T) => void;
 
-    const debounce = () => {
+export const useDebounce = function <Param extends any[]>(callback: Callback<Param>) {
+    const idRef = React.useRef<number>();
+    const callbackRef = React.useRef(callback);
+    callbackRef.current = callback;
+
+    const debounce = React.useCallback(() => {
         return function (...args: Param) {
             clearTimeout(idRef.current);
             idRef.current = window.setTimeout(() => {
-                callback(...args);
+                callbackRef.current(...args);
             }, 100);
         };
-    };
+    }, []);
 
     React.useEffect(() => () => clearTimeout(idRef.current), []);
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- callback is not allow to changed
-    return React.useMemo(() => debounce(), []);
+    return React.useMemo(() => debounce(), [debounce]);
 };
