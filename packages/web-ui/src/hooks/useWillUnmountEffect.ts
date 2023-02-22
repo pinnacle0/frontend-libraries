7f53cbdf937d8ee9126f895eb380605310ac3859
Alvis Tse <alvistse123@gmail.com>
2023-02-22 08:39:19 +0800

    [CHORE]: web-ui: typo

--- packages/web-ui/src/hooks/useWillUnmountEffect.ts
+++ packages/web-ui/src/hooks/useWillUnmountEffect.ts
@@ -3,6 +3,6 @@ import React from "react";
 export function useWillUnmountEffect(willUnmount: () => void) {
     React.useEffect(() => {
         return willUnmount;
-        // eslint-disable-next-line react-hooks/exhaustive-deps -- this is intentional to emulate the behaviour of componentWillUnmount
+        // eslint-disable-next-line react-hooks/exhaustive-deps -- this is intentional to emulate the behavior of componentWillUnmount
     }, []);
 }
