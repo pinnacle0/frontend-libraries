61c7c1b8459a2ee53773be7c626cd415a06e6ad8
Alvis Tse <alvistse123@gmail.com>
2023-02-24 10:02:57 +0800

    [REFACTOR]: web-ui: update useDebounce logic

--- packages/web-ui/src/hooks/useDebounce.ts
+++ packages/web-ui/src/hooks/useDebounce.ts
@@ -2,20 +2,19 @@ import React from "react";
 
 type Callback<T extends any[]> = (...args: T) => void;
 
-export const useDebounce = function <Param extends any[]>(callback: Callback<Param>, wait: number) {
+export function useDebounce<Param extends any[]>(callback: Callback<Param>, wait: number) {
     const idRef = React.useRef<number>();
+
     const callbackRef = React.useRef(callback);
     callbackRef.current = callback;
 
-    const debounce = React.useCallback(() => {
-        return function (...args: Param) {
-            clearTimeout(idRef.current);
-            idRef.current = window.setTimeout(() => {
-                callbackRef.current(...args);
-            }, wait);
-        };
-    }, [wait]);
+    const waitRef = React.useRef(wait);
+    waitRef.current = wait;
 
-    React.useEffect(() => () => clearTimeout(idRef.current), []);
-    return React.useMemo(() => debounce(), [debounce]);
-};
+    React.useEffect(() => () => window.clearTimeout(idRef.current), []);
+
+    return React.useCallback(function (...args: Param) {
+        window.clearTimeout(idRef.current);
+        idRef.current = window.setTimeout(() => callbackRef.current(...args), waitRef.current);
+    }, []);
+}
