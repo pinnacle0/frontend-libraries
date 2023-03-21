ce9b2123703bee5cfc1445104f5ccea671db9276
uay13211 <david19961113@gmail.com>
2023-03-21 14:35:13 +0800

    [FIX]: web-ui: fix wrong selectedRows and update with TS 5.0

--- packages/web-ui/src/core/VirtualTable/hooks/useRowSelection.tsx
+++ packages/web-ui/src/core/VirtualTable/hooks/useRowSelection.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import {Checkbox} from "../../Checkbox";
 import {ArrayUtil} from "../../../internal/ArrayUtil";
 import type {VirtualTableColumn, VirtualTableRowSelection} from "../type";
+import type {StringKey} from "../../../internal/type";
 
 interface Props<RowType extends object> {
     columns: VirtualTableColumn<RowType>[];
     dataSource: RowType[];
     rowSelection?: VirtualTableRowSelection<RowType>;
-    rowKey: "index" | React.Key;
+    rowKey: "index" | StringKey<RowType>;
 }
 
 export const useRowSelection = function <RowType extends object>({columns, dataSource, rowSelection, rowKey}: Props<RowType>) {
@@ -18,29 +19,32 @@ export const useRowSelection = function <RowType extends object>({columns, dataS
 
         const {width, onChange, selectedRowKeys, fixed, isDisabled, isSelectAllDisabled, title} = rowSelection;
 
-        const allSelectionRowKeys: React.Key[] = [];
+        const selectedRows: RowType[] = [];
+        const allSelectionRowKeys: (number | string)[] = [];
         const allSelectionRows: RowType[] = [];
-        const unAllSelectionRowKeys: React.Key[] = [];
+        const unAllSelectionRowKeys: (number | string)[] = [];
         const unAllSelectionRows: RowType[] = [];
-        const enabledRowKeys: React.Key[] = [];
-        const enabledCheckedRowKeys: React.Key[] = [];
+        const enabledRowKeys: (number | string)[] = [];
+        const enabledCheckedRowKeys: (number | string)[] = [];
 
         dataSource.forEach((data, rowIndex) => {
-            const key = rowKey === "index" ? rowIndex : data[rowKey];
+            const key = rowKey === "index" ? rowIndex : (data[rowKey] as string);
             const isSelected = selectedRowKeys.findIndex(_ => _ === key) !== -1;
             const isDisabledRow = isDisabled?.(data, rowIndex) || false;
             if (isDisabledRow) {
                 if (isSelected) {
+                    selectedRows.push(data);
                     allSelectionRowKeys.push(key);
-                    allSelectionRows.push(data[rowIndex]);
+                    allSelectionRows.push(data);
                     unAllSelectionRowKeys.push(key);
-                    unAllSelectionRows.push(data[rowIndex]);
+                    unAllSelectionRows.push(data);
                 }
             } else {
+                isSelected && selectedRows.push(data);
                 enabledRowKeys.push(key);
                 isSelected && enabledCheckedRowKeys.push(key);
                 allSelectionRowKeys.push(key);
-                allSelectionRows.push(data[rowIndex]);
+                allSelectionRows.push(data);
             }
         });
 
@@ -57,10 +61,10 @@ export const useRowSelection = function <RowType extends object>({columns, dataS
             fixed: fixed ? "left" : undefined,
             title: title || <Checkbox disabled={isAllSelectionDisabled} indeterminate={isIndeterminate} onChange={onSelectAll} value={isAllSelected} />,
             renderData: (data, rowIndex) => {
-                const key = rowKey === "index" ? rowIndex : data[rowKey];
-                const toggledSelectedRowKeys = ArrayUtil.toggleElement(selectedRowKeys, key);
-                const toggledSelectedRow = ArrayUtil.toggleElement(dataSource, data);
+                const key = rowKey === "index" ? rowIndex : (data[rowKey] as string);
                 const isChecked = selectedRowKeys.findIndex(_ => _ === key) !== -1;
+                const toggledSelectedRowKeys = ArrayUtil.toggleElement(selectedRowKeys, key);
+                const toggledSelectedRow = ArrayUtil.toggleElement(selectedRows, data);
                 return <Checkbox disabled={isDisabled?.(data, rowIndex)} value={isChecked} onChange={() => onChange(toggledSelectedRowKeys, toggledSelectedRow)} />;
             },
         };
