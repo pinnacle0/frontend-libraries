e9c5e5cca0fa3e923e96f589259c81f42e6f41e0
Alvis Tse <alvistse123@gmail.com>
2023-04-18 10:16:53 +0800

    [CHORE]: webpack-util: remove test code

--- packages/webpack-util/src/WebpackConfigGenerator/Plugin/ts.plugin.ts
+++ packages/webpack-util/src/WebpackConfigGenerator/Plugin/ts.plugin.ts
@@ -29,16 +29,3 @@ export function terserPlugin({sourceMap}: TerserPluginOptions): webpack.WebpackP
 export function reactRefreshPlugin(): webpack.WebpackPluginInstance {
     return WebpackConfigSerializationUtil.serializablePlugin("ReactRefreshPlugin", ReactRefreshWebpackPlugin);
 }
-
-const ctorToFn =
-    <A extends any[], R>(ctor: new (...args: A) => R) =>
-    (...args: A) =>
-        new ctor(...args);
-
-class NamedPlugin<T> {
-    constructor(public set: Array<T>) {}
-}
-
-const a = ctorToFn(NamedPlugin);
-
-a<number>([123, 123]);
