62df36d9c880daf24ae2f2e606c15a03a71783b6
Alvis Tse <alvistse123@gmail.com>
2023-05-05 15:42:30 +0800

    [FEATURE]: web-ui: add theme to StyleProvider

--- packages/web-ui/src/core/StyleProvider.tsx
+++ packages/web-ui/src/core/StyleProvider.tsx
@@ -2,21 +2,17 @@ import React from "react";
 import {ReactUtil} from "../util/ReactUtil";
 import {ConfigProvider} from "antd";
 import {StyleProvider as StyleProviderBase, legacyLogicalPropertiesTransformer} from "@ant-design/cssinjs";
+import type {ThemeConfig} from "antd";
 
 interface Props {
     children: React.ReactNode;
+    theme?: ThemeConfig;
 }
 
-export const StyleProvider = ReactUtil.memo("StyleProvider", ({children}: Props) => {
+export const StyleProvider = ReactUtil.memo("StyleProvider", ({children, theme}: Props) => {
     return (
         <StyleProviderBase hashPriority="high" transformers={[legacyLogicalPropertiesTransformer]}>
-            <ConfigProvider
-                theme={{
-                    token: {borderRadius: 5},
-                }}
-            >
-                {children}
-            </ConfigProvider>
+            <ConfigProvider theme={theme}>{children}</ConfigProvider>
         </StyleProviderBase>
     );
 });
