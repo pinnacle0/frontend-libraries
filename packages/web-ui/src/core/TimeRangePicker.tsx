16c5987c4eca9e1783abc8ce68a9873ba06c9d77
Alvis Tse <alvistse123@gmail.com>
2023-05-05 18:27:43 +0800

    [REFACTOR]: web-ui: remove TimeRangePicker style

--- packages/web-ui/src/core/TimeRangePicker.tsx
+++ packages/web-ui/src/core/TimeRangePicker.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import dayjs from "dayjs";
 import DatePicker from "antd/es/date-picker";
 import type {Dayjs} from "dayjs";
-import type {ControlledFormValue} from "../../internal/type";
-import "./index.less";
+import type {ControlledFormValue} from "../internal/type";
 
 export interface Props<T extends boolean> extends ControlledFormValue<T extends false ? [string, string] : [string | null, string | null]> {
     allowNull: T;
