b681abd0c8eb19f2ae18fb8cdecf39e878894b43
Alvis Tse <alvistse123@gmail.com>
2023-05-08 10:00:50 +0800

    [REFACTOR]: web-ui: export type in style provider

--- packages/web-ui/src/core/StyleProvider.tsx
+++ packages/web-ui/src/core/StyleProvider.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {ReactUtil} from "../util/ReactUtil";
-import {ConfigProvider} from "antd";
+import ConfigProvider from "antd/es/config-provider";
 import {StyleProvider as StyleProviderBase, legacyLogicalPropertiesTransformer} from "@ant-design/cssinjs";
 import type {ThemeConfig} from "antd";
 
-interface Props {
+export type Theme = ThemeConfig;
+
+export interface Props {
     children: React.ReactNode;
-    theme?: ThemeConfig;
+    theme?: Theme;
 }
 
 export const StyleProvider = ReactUtil.memo("StyleProvider", ({children, theme}: Props) => {
