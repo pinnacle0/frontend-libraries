b23a2edba3bfd877432ab3e1390664dcc79dc13f
pinnaclechuen <chuen.tam@pinnacle-holdings.hk>
2023-05-08 18:56:05 +0800

    [REFACTOR]: web-ui: core/Button add g-button class

--- packages/web-ui/src/core/Button.tsx
+++ packages/web-ui/src/core/Button.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import AntButton from "antd/es/button";
 import type {ButtonProps} from "antd/es/button";
+import classNames from "classnames";
 
 export interface Props extends ButtonProps {}
 
-export function Button(props: Props) {
-    return <AntButton {...props} />;
+export function Button({className, ...rest}: Props) {
+    return <AntButton className={classNames("g-button", className)} {...rest} />;
 }
 
 export type {ButtonSize, ButtonShape, ButtonType, ButtonHTMLType} from "antd/es/button";
