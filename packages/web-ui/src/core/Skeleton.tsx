aaafeeab699b0acb3065d27e35472c792c1b0f00
Alvis Tse <alvistse123@gmail.com>
2023-05-09 13:49:16 +0800

    [REFACTOR]: web-ui: add g-skeleton to Skeleton className

--- packages/web-ui/src/core/Skeleton.tsx
+++ packages/web-ui/src/core/Skeleton.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type {SkeletonProps} from "antd/es/skeleton";
 import AntSkeleton from "antd/es/skeleton";
+import {classNames} from "../util/ClassNames";
 
 export interface Props extends SkeletonProps {}
 
@@ -8,6 +9,7 @@ export class Skeleton extends React.PureComponent<Props> {
     static displayName = "Skeleton";
 
     render() {
-        return <AntSkeleton {...this.props} />;
+        const {className, ...restProps} = this.props;
+        return <AntSkeleton className={classNames("g-skeleton", className)} {...restProps} />;
     }
 }
