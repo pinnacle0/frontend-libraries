4a9aeb8c15d1c353b20bce115a13a28e62eaa989
pinnaclechuen <chuen.tam@pinnacle-holdings.hk>
2023-06-19 14:28:41 +0800

    [REFACTOR]: web-ui/core export ConfigProvider

---
+++ packages/web-ui/src/core/ConfigProvider.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {ReactUtil} from "../util/ReactUtil";
+import AntdConfigProvider, {ConfigProviderProps} from "antd/es/config-provider";
+import type {ThemeConfig} from "antd";
+
+export type Theme = ThemeConfig;
+
+export interface Props extends ConfigProviderProps {}
+
+export const ConfigProvider = ReactUtil.memo("ConfigProvider", (props: Props) => {
+    return <AntdConfigProvider {...props} />;
+});
