2caf588678a252dd00a9785f041272f11c9c840f
Alvis Tse <alvistse123@gmail.com>
2023-06-20 10:34:18 +0800

    [REFACTOR]: web-ui: remove ConfigProvider inside StyleProvider

--- packages/web-ui/src/core/StyleProvider.tsx
+++ packages/web-ui/src/core/StyleProvider.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {ReactUtil} from "../util/ReactUtil";
-import ConfigProvider from "antd/es/config-provider";
 import {StyleProvider as StyleProviderBase, legacyLogicalPropertiesTransformer} from "@ant-design/cssinjs";
 import type {ThemeConfig} from "antd";
 
@@ -8,13 +7,12 @@ export type Theme = ThemeConfig;
 
 export interface Props {
     children: React.ReactNode;
-    theme?: Theme;
 }
 
-export const StyleProvider = ReactUtil.memo("StyleProvider", ({children, theme}: Props) => {
+export const StyleProvider = ReactUtil.memo("StyleProvider", ({children}: Props) => {
     return (
         <StyleProviderBase hashPriority="high" transformers={[legacyLogicalPropertiesTransformer]}>
-            <ConfigProvider theme={theme}>{children}</ConfigProvider>
+            {children}
         </StyleProviderBase>
     );
 });
