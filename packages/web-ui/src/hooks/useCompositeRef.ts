afa8f3e9b8a2df3c6b52def7e57b270d5545d77b
uay13211 <david19961113@gmail.com>
2023-06-26 17:24:55 +0800

    [REFACTOR]: web-ui: fix useCompositeRef type

--- packages/web-ui/src/hooks/useCompositeRef.ts
+++ packages/web-ui/src/hooks/useCompositeRef.ts
@@ -2,7 +2,7 @@ import React from "react";
 
 export function useCompositeRef(...refs: Array<React.MutableRefObject<any> | React.RefCallback<any> | undefined | null>) {
     const refListRef = React.useRef(refs);
-    return React.useCallback((node: Node) => {
+    return React.useCallback((node: Node | null) => {
         refListRef.current.forEach(ref => {
             if (!ref) return;
             if (typeof ref === "function") {
