e71f41bfce18a09e89e33c54723124035515f7d7
Shad02w <alvistse123@gmail.com>
2023-06-27 21:50:19 +0800

    [REFACTOR]: web-ui: remove Theme type export in StyleProvider

--- packages/web-ui/src/core/StyleProvider.tsx
+++ packages/web-ui/src/core/StyleProvider.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import {ReactUtil} from "../util/ReactUtil";
 import {StyleProvider as StyleProviderBase, legacyLogicalPropertiesTransformer} from "@ant-design/cssinjs";
-import type {ThemeConfig} from "antd";
-
-export type Theme = ThemeConfig;
 
 export interface Props {
     children: React.ReactNode;
