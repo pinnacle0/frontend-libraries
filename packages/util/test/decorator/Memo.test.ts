b509da464632762507041da59c7c4c7f0dc78f75
Shad02w <alvistse123@gmail.com>
2023-07-09 12:59:22 +0800

    [REFACTOR]: util: test @Memo on multiple instance

--- packages/util/test/decorator/Memo.test.ts
+++ packages/util/test/decorator/Memo.test.ts
@@ -1,61 +1,95 @@
 import {Memo} from "../../src/decorator/Memo";
 
-test("@Memo", () => {
-    const mock = jest.fn();
-    class Person {
-        constructor(private name: string, private age: number) {}
-        @Memo
-        getName(...args: any[]) {
-            mock();
-            return {args, name: this.name};
+describe("@Memo", () => {
+    test("should memoize function result when parameter is the same", () => {
+        const mock = jest.fn();
+        class Person {
+            constructor(private name: string, private age: number) {}
+            @Memo
+            getName(...args: any[]) {
+                mock();
+                return {args, name: this.name};
+            }
+
+            @Memo
+            getAge(...args: any[]) {
+                mock();
+                return {args, age: this.age};
+            }
         }
 
-        @Memo
-        getAge(...args: any[]) {
-            mock();
-            return {args, age: this.age};
+        const mary = new Person("Mary", 18);
+
+        // void
+        let result: any = mary.getName();
+        expect(mock).toBeCalledTimes(1);
+        expect(result).toStrictEqual({args: [], name: "Mary"});
+
+        result = mary.getName();
+        expect(mock).toBeCalledTimes(1);
+        expect(result).toStrictEqual({args: [], name: "Mary"});
+
+        // number
+        result = mary.getName(123);
+        expect(mock).toBeCalledTimes(2);
+        expect(result).toStrictEqual({args: [123], name: "Mary"});
+
+        result = mary.getName(123);
+        expect(mock).toBeCalledTimes(2);
+        expect(result).toStrictEqual({args: [123], name: "Mary"});
+
+        let object: Record<string, number> = {a: 1};
+        result = mary.getName(object);
+        expect(mock).toBeCalledTimes(3);
+        expect(result).toStrictEqual({args: [{a: 1}], name: "Mary"});
+
+        result = mary.getName(object);
+        expect(mock).toBeCalledTimes(3);
+        expect(result).toStrictEqual({args: [{a: 1}], name: "Mary"});
+
+        result = mary.getName(123);
+        expect(mock).toBeCalledTimes(3);
+        expect(result).toStrictEqual({args: [123], name: "Mary"});
+
+        object = {...object, b: 2};
+        result = mary.getName(object);
+        expect(mock).toBeCalledTimes(4);
+        expect(result).toStrictEqual({args: [{a: 1, b: 2}], name: "Mary"});
+
+        result = mary.getAge(123);
+        expect(mock).toBeCalledTimes(5);
+        expect(result).toStrictEqual({args: [123], age: 18});
+    });
+
+    test("should not affect return value when creating another instance", () => {
+        const mock = jest.fn();
+        class Person {
+            constructor(private name: string) {}
+
+            @Memo
+            getInfo(age: number) {
+                mock();
+                return `name: ${this.name}, age: ${age}`;
+            }
         }
-    }
 
-    const mary = new Person("Mary", 18);
+        const mary = new Person("Mary");
 
-    // void
-    let result: any = mary.getName();
-    expect(mock).toBeCalledTimes(1);
-    expect(result).toStrictEqual({args: [], name: "Mary"});
+        let result = mary.getInfo(30);
+        expect(mock).toBeCalledTimes(1);
+        expect(result).toBe("name: Mary, age: 30");
 
-    result = mary.getName();
-    expect(mock).toBeCalledTimes(1);
-    expect(result).toStrictEqual({args: [], name: "Mary"});
+        result = mary.getInfo(30);
+        expect(mock).toBeCalledTimes(1);
+        expect(result).toBe("name: Mary, age: 30");
 
-    // number
-    result = mary.getName(123);
-    expect(mock).toBeCalledTimes(2);
-    expect(result).toStrictEqual({args: [123], name: "Mary"});
+        const peter = new Person("Peter");
+        result = peter.getInfo(30);
+        expect(mock).toBeCalledTimes(2);
+        expect(result).toBe("name: Peter, age: 30");
 
-    result = mary.getName(123);
-    expect(mock).toBeCalledTimes(2);
-    expect(result).toStrictEqual({args: [123], name: "Mary"});
-
-    let object: Record<string, number> = {a: 1};
-    result = mary.getName(object);
-    expect(mock).toBeCalledTimes(3);
-    expect(result).toStrictEqual({args: [{a: 1}], name: "Mary"});
-
-    result = mary.getName(object);
-    expect(mock).toBeCalledTimes(3);
-    expect(result).toStrictEqual({args: [{a: 1}], name: "Mary"});
-
-    result = mary.getName(123);
-    expect(mock).toBeCalledTimes(3);
-    expect(result).toStrictEqual({args: [123], name: "Mary"});
-
-    object = {...object, b: 2};
-    result = mary.getName(object);
-    expect(mock).toBeCalledTimes(4);
-    expect(result).toStrictEqual({args: [{a: 1, b: 2}], name: "Mary"});
-
-    result = mary.getAge(123);
-    expect(mock).toBeCalledTimes(5);
-    expect(result).toStrictEqual({args: [123], age: 18});
+        result = mary.getInfo(30);
+        expect(mock).toBeCalledTimes(2);
+        expect(result).toBe("name: Mary, age: 30");
+    });
 });
