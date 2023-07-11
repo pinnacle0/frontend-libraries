fc74c64d90ad61f84daa5605720c333ec552f5e5
uay13211 <david19961113@gmail.com>
2023-07-11 11:18:59 +0800

    [REFACTOR]: web-ui: update PagedList and add onPageChange props

--- packages/web-ui/src/core/PagedList/index.tsx
+++ packages/web-ui/src/core/PagedList/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {ReactUtil} from "../../util/ReactUtil";
 import {classNames} from "../../util/ClassNames";
 import {i18n} from "../../internal/i18n/core";
 import type {StringKey} from "../../internal/type";
@@ -10,40 +11,29 @@ export interface Props<T extends object> {
     renderItem: React.ComponentType<{item: T; index: number}>;
     itemWidth: number;
     pageSize: number;
+    onPageChange?: (pageIndex: number) => void;
 }
 
-interface State {
-    currentItemIndex: number;
-}
+export const PagedList = ReactUtil.memo("PagedList", <T extends object>({dataSource, rowKey, renderItem, itemWidth, pageSize, onPageChange}: Props<T>) => {
+    const [currentItemIndex, setCurrentItemIndex] = React.useState(0);
+    const t = i18n();
+    const Component = renderItem;
+    const isNextDisabled = currentItemIndex === 0;
+    const upperLimit = dataSource.length - pageSize;
+    const isPreviousDisabled = currentItemIndex >= upperLimit;
+    const currentPageIndex = React.useMemo(() => Math.floor(currentItemIndex / pageSize), [currentItemIndex, pageSize]);
 
-export class PagedList<T extends object> extends React.PureComponent<Props<T>, State> {
-    static displayName = "PagedList";
-
-    constructor(props: Props<T>) {
-        super(props);
-        this.state = {
-            currentItemIndex: 0,
-        };
-    }
-
-    getUpperLimit = () => this.props.dataSource.length - this.props.pageSize;
-
-    goPreviousPage = () => {
-        const {pageSize} = this.props;
-        const {currentItemIndex} = this.state;
-        this.setState({currentItemIndex: Math.max(0, Math.min(currentItemIndex + pageSize, this.getUpperLimit()))});
+    const goPreviousPage = () => {
+        setCurrentItemIndex(Math.max(0, Math.min(currentItemIndex + pageSize, upperLimit)));
     };
 
-    goNextPage = () => {
-        const {pageSize} = this.props;
-        const {currentItemIndex} = this.state;
-        this.setState({currentItemIndex: Math.max(0, currentItemIndex - pageSize)});
+    const goNextPage = () => {
+        setCurrentItemIndex(Math.max(0, currentItemIndex - pageSize));
     };
 
-    goCurrentPage = () => this.setState({currentItemIndex: 0});
+    const goCurrentPage = () => setCurrentItemIndex(0);
 
-    getRowKey = (record: T, index: number): string | number => {
-        const {rowKey} = this.props;
+    const getRowKey = (record: T, index: number): string | number => {
         if (rowKey === "index") {
             return index;
         } else if (typeof rowKey === "function") {
@@ -53,41 +43,36 @@ export class PagedList<T extends object> extends React.PureComponent<Props<T>, S
         }
     };
 
-    render() {
-        const {dataSource, renderItem, itemWidth, pageSize} = this.props;
-        const {currentItemIndex} = this.state;
-        const isNextDisabled = currentItemIndex === 0;
-        const isPreviousDisabled = currentItemIndex >= this.getUpperLimit();
-        const t = i18n();
-        const Component = renderItem;
+    React.useEffect(() => {
+        onPageChange?.(currentPageIndex);
+    }, [currentPageIndex, onPageChange]);
 
-        return (
-            <div className="g-paged-list">
-                <div className="list-wrap" style={{width: itemWidth * pageSize}}>
-                    <div className="list" style={{transform: `translateX(${currentItemIndex * itemWidth}px)`}}>
-                        {dataSource.map((_, index) => (
-                            <div style={{flex: `0 0 ${itemWidth}px`}} key={this.getRowKey(_, index)}>
-                                <Component index={index} item={_} />
-                            </div>
-                        ))}
-                    </div>
-                    {dataSource.length === 0 && <div className="no-data">{t.noData}</div>}
+    return (
+        <div className="g-paged-list">
+            <div className="list-wrap" style={{width: itemWidth * pageSize}}>
+                <div className="list" style={{transform: `translateX(${currentItemIndex * itemWidth}px)`}}>
+                    {dataSource.map((_, index) => (
+                        <div style={{flex: `0 0 ${itemWidth}px`}} key={getRowKey(_, index)}>
+                            <Component index={index} item={_} />
+                        </div>
+                    ))}
                 </div>
-                <div className="navigation">
-                    <div onClick={isNextDisabled ? undefined : this.goNextPage} className={classNames({disabled: isNextDisabled})}>
-                        <div className="next-button" />
-                        <div>{t.nextPage}</div>
-                    </div>
-                    <div onClick={isPreviousDisabled ? undefined : this.goPreviousPage} className={isPreviousDisabled ? "disabled" : ""}>
-                        <div className="previous-button" />
-                        <div>{t.prevPage}</div>
-                    </div>
-                    <div onClick={isNextDisabled ? undefined : this.goCurrentPage} className={classNames({disabled: isNextDisabled})}>
-                        <div className="current-button" />
-                        <div>{t.currentPage}</div>
-                    </div>
+                {dataSource.length === 0 && <div className="no-data">{t.noData}</div>}
+            </div>
+            <div className="navigation">
+                <div onClick={isNextDisabled ? undefined : goNextPage} className={classNames({disabled: isNextDisabled})}>
+                    <div className="next-button" />
+                    <div>{t.nextPage}</div>
+                </div>
+                <div onClick={isPreviousDisabled ? undefined : goPreviousPage} className={isPreviousDisabled ? "disabled" : ""}>
+                    <div className="previous-button" />
+                    <div>{t.prevPage}</div>
+                </div>
+                <div onClick={isNextDisabled ? undefined : goCurrentPage} className={classNames({disabled: isNextDisabled})}>
+                    <div className="current-button" />
+                    <div>{t.currentPage}</div>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+});
