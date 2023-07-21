f5eecad2234193edbd803da08a46c0e27676e43d
Alvis Tse <alvistse123@gmail.com>
2023-07-21 09:59:10 +0800

    [CHORE]: react-stack-router: minor update

--- packages/react-stack-router/src/index.ts
+++ packages/react-stack-router/src/index.ts
@@ -1,4 +1,4 @@
 export {createRouter} from "./createRouter";
-export type {HistoryState, Router, PushOption, ReplaceOption, TransitionOption} from "./type";
 export {Link} from "./component/Link";
 export * from "./hooks";
+export type {HistoryState, Router, PushOption, ReplaceOption, TransitionOption} from "./type";
