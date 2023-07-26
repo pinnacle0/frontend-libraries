e1102afa1ccd18d186ad586be258e1b85e22e307
Alvis Tse <alvistse123@gmail.com>
2023-07-26 16:34:40 +0800

    [CHORE]: react-stack-router: add ssl to vite dev server, for local webclip testing

--- packages/react-stack-router/demo/vite.config.ts
+++ packages/react-stack-router/demo/vite.config.ts
@@ -1,9 +1,11 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
+import basicSSL from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
     server: {
+        https: true,
         port: 7444,
     },
-    plugins: [react()],
+    plugins: [react(), basicSSL() ],
 });
