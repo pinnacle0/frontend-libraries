912740445a11b379e6f3181429aec85529bebc4d
Alvis Tse <alvistse123@gmail.com>
2023-07-26 19:03:56 +0800

    [REFACTOR]: react-stack-router: stack screen style

--- packages/react-stack-router/src/component/Stack/index.tsx
+++ packages/react-stack-router/src/component/Stack/index.tsx
@@ -1,10 +1,11 @@
-import {useEffect, useState} from "react";
+import {useEffect, useLayoutEffect, useState} from "react";
 import classNames from "classnames";
 import {AnimatePresence, Animated} from "../Animated";
 import {RouteContext} from "../../context";
 import type {CSSProperties} from "react";
 import type {Screen} from "../../screen";
 import type {StackRouter} from "../../stackRouter";
+import "./index.less";
 
 interface StackProps {
     router: StackRouter;
@@ -14,16 +15,26 @@ interface StackProps {
 
 export function Stack({router, className, style}: StackProps) {
     const [screens, setScreens] = useState<Screen[]>([]);
-
     useEffect(() => router.subscribe(_ => setScreens([..._])), [router]);
+
+    useLayoutEffect(() => {
+        if (screens.length > 1) {
+            const original = document.body.style.overflowY;
+            document.body.style.overflowY = "hidden";
+            return () => {
+                document.body.style.overflowY = original;
+            };
+        }
+    }, [screens]);
 
     return (
         <div className={classNames("g-stack-router", className)} style={style}>
             <AnimatePresence>
-                {screens.map(screen => {
+                {screens.map((screen, index) => {
                     const context: RouteContext = {location: screen.history.location, lifecycle: screen.lifecycle, params: screen.history.params};
                     return (
                         <Animated.div
+                            className={classNames("g-stack-router-screen", {overlay: index > 0})}
                             key={screen.history.location.key}
                             enter={() => screen.transition.enteringKeyframes}
                             exit={() => screen.transition.exitingKeyframes}
