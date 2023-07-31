1f3ed420148d34f1eb3d7b8b3f9968986bbfc510
Shad02w <alvistse123@gmail.com>
2023-07-31 20:50:08 +0800

    [REFACTOR]: react-stack-router: export Location and History type

--- packages/react-stack-router/src/index.ts
+++ packages/react-stack-router/src/index.ts
@@ -2,6 +2,7 @@ export {createRouter} from "./createRouter";
 export {Link} from "./component/Link";
 export {RouterContext, RouteContext} from "./context";
 export * from "./hooks";
+export type {History} from "history";
 export type {RouteProps, PathRouteProps, ComponentRouteProps} from "./component/Route";
-export type {LocationState as HistoryState, Router, PushOption, ReplaceOption, TransitionOption} from "./type";
+export type {LocationState as HistoryState, Location, Router, PushOption, ReplaceOption, TransitionOption} from "./type";
 export type {LifecycleHook} from "./screen/lifecycle";
