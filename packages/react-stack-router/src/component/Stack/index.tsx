eb0e866383f59c97fa2090d582bb766aad8faaf4
Shad02w <alvistse123@gmail.com>
2023-08-02 01:48:47 +0800

    [REFACTOR]: react-stack-router: exiting animation

--- packages/react-stack-router/src/component/Stack/index.tsx
+++ packages/react-stack-router/src/component/Stack/index.tsx
@@ -1,8 +1,8 @@
-import {useEffect, useLayoutEffect, useState} from "react";
+import {useEffect, useLayoutEffect, useRef, useState} from "react";
 import classNames from "classnames";
 import {AnimatePresence, Animated} from "../Animated";
 import {RouteContext} from "../../context";
-import type {CSSProperties} from "react";
+import type {CSSProperties, ElementRef} from "react";
 import type {Screen} from "../../screen";
 import type {StackRouter} from "../../stackRouter";
 import "./index.less";
@@ -15,6 +15,7 @@ interface StackProps {
 
 export function Stack({router, className, style}: StackProps) {
     const [screens, setScreens] = useState<Screen[]>([]);
+    const [exiting, setExiting] = useState(0);
     useEffect(() => router.subscribe(_ => setScreens([..._])), [router]);
 
     useLayoutEffect(() => {
@@ -28,7 +29,7 @@ export function Stack({router, className, style}: StackProps) {
     }, [screens]);
 
     return (
-        <div className={classNames("g-stack-router", className)} style={style}>
+        <div className={classNames("g-stack-router", {exiting: exiting !== 0}, className)} style={style}>
             <AnimatePresence>
                 {screens.map((screen, index) => {
                     const context: RouteContext = {location: screen.history.location, lifecycle: screen.lifecycle, params: screen.history.params};
@@ -40,8 +41,14 @@ export function Stack({router, className, style}: StackProps) {
                             exit={() => screen.transition.exitingKeyframes}
                             onEntering={() => screen.lifecycle.trigger("willEnter")}
                             onEntered={() => screen.lifecycle.trigger("didEnter")}
-                            onExiting={() => screen.lifecycle.trigger("willExit")}
-                            onExited={() => screen.lifecycle.trigger("didExit")}
+                            onExiting={() => {
+                                setExiting(_ => _ + 1);
+                                screen.lifecycle.trigger("willExit");
+                            }}
+                            onExited={() => {
+                                setExiting(_ => _ - 1);
+                                screen.lifecycle.trigger("didExit");
+                            }}
                         >
                             <RouteContext.Provider value={context}>
                                 <screen.content />
