5e3bcf66ddc499d143cc31b601d90c5ece8f4535
Alvis Tse <alvistse123@gmail.com>
2023-08-01 11:48:44 +0800

    [CHORE]: react-stack-router: rename hooks to hook

--- packages/react-stack-router/src/index.ts
+++ packages/react-stack-router/src/index.ts
@@ -1,7 +1,7 @@
 export {createRouter} from "./createRouter";
 export {Link} from "./component/Link";
 export {RouterContext, RouteContext} from "./context";
-export * from "./hooks";
+export * from "./hook";
 export type {History} from "history";
 export type {RouteProps, PathRouteProps, ComponentRouteProps} from "./component/Route";
 export type {LocationState as HistoryState, Location, Router, PushOption, ReplaceOption, TransitionOption} from "./type";
