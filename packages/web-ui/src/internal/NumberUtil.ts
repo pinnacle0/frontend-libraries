86e8b82f34cfe91857ebb6a39238be4aa70294f4
uay13211 <david19961113@gmail.com>
2023-12-11 16:52:12 +0800

    [FIX]: web-ui: update internal NumberUtil.rounding for precise rounding

--- packages/web-ui/src/internal/NumberUtil.ts
+++ packages/web-ui/src/internal/NumberUtil.ts
@@ -1,7 +1,7 @@
 function rounding(value: number, algorithm: "round" | "ceil" | "floor", maxScale: number): number {
-    if (!Number.isInteger(maxScale) || maxScale < 0 || maxScale > 10) {
-        throw new Error("[web-ui] NumberUtil.rounding maxScale must be an integer in range [0, 10]");
-    }
+    if (!Number.isInteger(maxScale) || maxScale < 0 || maxScale > 10) throw new Error("[web-ui] NumberUtil.rounding: maxScale must be an integer between [0, 10]");
+    if (!Number.isFinite(value)) return value;
+
     /**
      * Take "4.975" as an example.
      * First, we split the number by the decimal point to get ["4", "975"].
@@ -15,17 +15,19 @@ function rounding(value: number, algorithm: "round" | "ceil" | "floor", maxScale
      * Therefore, we first get 4.975 * 10^3 = 4975. Then, we get 4975 / 10^(3 - 2) = 497.5 and we round this number.
      * In the end, we divide this number by 10^maxScale to obtained the final answer, i.e. 498 / 10^2 = 4.98 (assume algorithm is "round").
      */
-    const parts = Number(value).toString().split(".");
-    const adjustmentScale = parts.length < 2 ? 0 : parts[1].length;
-    const adjustmentPowerOf10 = 10 ** adjustmentScale;
-    const isAdjustmentSafe = Number(value) * adjustmentPowerOf10 <= Number.MAX_SAFE_INTEGER;
-    const powerOf10 = 10 ** maxScale;
-    if (adjustmentScale > maxScale && isAdjustmentSafe) {
-        const diffPowerOf10 = 10 ** (adjustmentScale - maxScale);
-        return Math[algorithm]((value * adjustmentPowerOf10) / diffPowerOf10) / powerOf10;
-    } else {
-        return Math[algorithm](value * powerOf10) / powerOf10;
+
+    // for passing exponential notation string to it
+    const func = Math[algorithm] as any;
+
+    const precision = maxScale == null ? 0 : maxScale >= 0 ? Math.min(maxScale, 292) : Math.max(maxScale, -292);
+    if (precision) {
+        // Shift with exponential notation to avoid floating-point issues.
+        let pair = `${value}e`.split("e");
+        const roundedValue = func(`${pair[0]}e${+pair[1] + precision}`);
+        pair = `${roundedValue}e`.split("e");
+        return +`${pair[0]}e${+pair[1] - precision}`;
     }
+    return func(value);
 }
 
 function roundingToString(value: number, algorithm: "round" | "ceil" | "floor", scale: number): string {
