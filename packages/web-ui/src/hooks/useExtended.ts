9a39788860675c4375ed0b7c35edda089a46cb08
uay13211 <david19961113@gmail.com>
2024-05-09 14:44:20 +0800

    [REFACTOR]: web-ui: update useExtendedWhen logic

--- packages/web-ui/src/hooks/useExtended.ts
+++ packages/web-ui/src/hooks/useExtended.ts
@@ -3,25 +3,30 @@ import {useForceUpdate} from "./useForceUpdate";
 
 export function useExtendedWhen<T>(value: T, when: (value: T) => boolean, minDuration: number) {
     const forceUpdate = useForceUpdate();
-    const extendedRef = React.useRef<T>(value);
+    const currentValueRef = React.useRef<T>(value);
+    const extendedValueRef = React.useRef<T>(value);
     const lastValueUpdateTimeRef = React.useRef(0);
+    const timerRef = React.useRef<number | null>(null);
 
-    const elapsed = Date.now() - lastValueUpdateTimeRef.current;
+    currentValueRef.current = value;
 
-    if (lastValueUpdateTimeRef.current > 0 && elapsed < minDuration) {
-        window.setTimeout(() => {
-            extendedRef.current = value;
-            lastValueUpdateTimeRef.current = 0;
-            forceUpdate();
-        }, minDuration - elapsed);
-    } else {
-        if (lastValueUpdateTimeRef.current === 0 && when(value)) {
-            lastValueUpdateTimeRef.current = Date.now();
-            extendedRef.current = value;
+    if (timerRef.current === null) {
+        const elapsed = Date.now() - lastValueUpdateTimeRef.current;
+        if (lastValueUpdateTimeRef.current > 0 && elapsed < minDuration) {
+            timerRef.current = window.setTimeout(() => {
+                extendedValueRef.current = currentValueRef.current;
+                lastValueUpdateTimeRef.current = 0;
+                timerRef.current = null;
+                forceUpdate();
+            }, minDuration - elapsed);
+        } else {
+            if (when(value)) {
+                lastValueUpdateTimeRef.current = Date.now();
+            }
+            extendedValueRef.current = value;
         }
     }
-
-    return extendedRef.current;
+    return extendedValueRef.current;
 }
 
 export function useExtended<T>(value: T, minDuration: number) {
--- packages/web-ui/test/unit-test/hook/useExtended.test.ts
+++ packages/web-ui/test/unit-test/hook/useExtended.test.ts
@@ -1,6 +1,7 @@
 import {act, renderHook} from "@testing-library/react";
 import {useBool} from "../../../src/hooks/useBool";
 import {useExtendedWhen} from "../../../src/hooks/useExtended";
+import React from "react";
 
 describe("useExtendedWhen unit testing", () => {
     afterEach(() => {
@@ -69,5 +70,59 @@ describe("useExtendedWhen unit testing", () => {
 
         expect(result.current.show).toBeFalsy();
         expect(result.current.extendedShow).toBeFalsy();
+
+        act(() => result.current.open());
+
+        expect(result.current.show).toBeTruthy();
+        expect(result.current.extendedShow).toBeTruthy();
+
+        act(() => result.current.close());
+
+        expect(result.current.show).toBeFalsy();
+        expect(result.current.extendedShow).toBeTruthy();
+
+        act(() => jest.runAllTimers());
+
+        expect(result.current.show).toBeFalsy();
+        expect(result.current.extendedShow).toBeFalsy();
+    });
+
+    it("should only show the latest value after minDuration", () => {
+        const {result} = renderHook(() => {
+            const [value, setValue] = React.useState(0);
+            const extendedValue = useExtendedWhen(value, value => value === 3, 500);
+
+            return {value, setValue, extendedValue};
+        });
+
+        expect(result.current.value).toBe(0);
+        expect(result.current.extendedValue).toBe(0);
+
+        act(() => result.current.setValue(1));
+
+        expect(result.current.value).toBe(1);
+        expect(result.current.extendedValue).toBe(1);
+
+        act(() => result.current.setValue(3));
+
+        expect(result.current.value).toBe(3);
+        expect(result.current.extendedValue).toBe(3);
+
+        act(() => result.current.setValue(4));
+
+        expect(result.current.value).toBe(4);
+        expect(result.current.extendedValue).toBe(3);
+
+        act(() => result.current.setValue(5));
+        act(() => result.current.setValue(6));
+        act(() => result.current.setValue(7));
+
+        expect(result.current.value).toBe(7);
+        expect(result.current.extendedValue).toBe(3);
+
+        act(() => jest.runAllTimers());
+
+        expect(result.current.value).toBe(7);
+        expect(result.current.extendedValue).toBe(7);
     });
 });
