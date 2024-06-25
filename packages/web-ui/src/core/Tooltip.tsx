ce9488a685a325e50b1446fe34d38cf14e859cb5
ianlau <ianlauyin@gmail.com>
2024-06-25 13:20:42 +0800

    [FIX]: web-ui: update Tooltip with ReactUtil.memo instead of React.memo

--- packages/web-ui/src/core/Tooltip.tsx
+++ packages/web-ui/src/core/Tooltip.tsx
@@ -1,11 +1,12 @@
 import type {TooltipPlacement, TooltipProps} from "antd/es/tooltip";
 import AntTooltip from "antd/es/tooltip";
 import React from "react";
+import {ReactUtil} from "../util/ReactUtil";
 
 // Cannot use interface extends here because TooltipProps is a type union
 export type Props = TooltipProps;
 
-export const Tooltip = React.memo((props: TooltipProps) => {
+export const Tooltip = ReactUtil.memo("Tooltip",(props: TooltipProps) => {
     return (
         <AntTooltip {...props}>
             <Children {...props}/>
