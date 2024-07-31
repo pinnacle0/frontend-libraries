598ab93f341eb2c3ae0389158cb5e69133e35468
Anthony Lee <anthonylee1994@users.noreply.github.com>
2024-07-31 18:38:11 +0800

    [UPDATE]: web-ui: update to 0.5.74 for dayjs

--- packages/web-ui/src/util/DayJS.ts
+++ packages/web-ui/src/util/DayJS.ts
@@ -1,2 +1,16 @@
 import dayjs, {type Dayjs} from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import localeData from "dayjs/plugin/localeData";
+import weekday from "dayjs/plugin/weekday";
+import weekOfYear from "dayjs/plugin/weekOfYear";
+import weekYear from "dayjs/plugin/weekYear";
+
+dayjs.extend(customParseFormat);
+dayjs.extend(advancedFormat);
+dayjs.extend(weekday);
+dayjs.extend(localeData);
+dayjs.extend(weekOfYear);
+dayjs.extend(weekYear);
+
 export {dayjs, Dayjs};
