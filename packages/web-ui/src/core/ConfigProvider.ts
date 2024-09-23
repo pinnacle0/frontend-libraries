f6dd4a3582af73322ba1fdbfeae9a9055b1bb1a1
uay13211 <david19961113@gmail.com>
2024-09-23 16:34:38 +0800

    [REFACTOR]: web-ui: merge StyleProvider, ConfigProvider, LocaleProvider into UIProvider

--- packages/web-ui/src/core/ConfigProvider.ts
+++
@@ -1,4 +0,0 @@
-export type {ThemeConfig} from "antd";
-
-export {default as ConfigProvider} from "antd/es/config-provider";
-export type {ConfigProviderProps as Props} from "antd/es/config-provider";
---
+++ packages/web-ui/src/core/UIProvider.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import chineseLocale from "antd/es/locale/zh_CN";
+import englishLocale from "antd/es/locale/en_US";
+import {ReactUtil} from "../util/ReactUtil";
+import {StyleProvider, legacyLogicalPropertiesTransformer} from "@ant-design/cssinjs";
+import {ConfigProvider} from "antd";
+import type {Locale} from "../util/LocaleUtil";
+import type {ConfigProviderProps} from "antd/es/config-provider";
+import {useDidMountEffect} from "../hooks/useDidMountEffect";
+import {LocaleContext, LocaleUtil} from "../util/LocaleUtil";
+
+export type {ThemeConfig} from "antd";
+
+interface Props extends Omit<ConfigProviderProps, "locale"> {
+    children: React.ReactNode;
+    locale: Locale | "auto";
+}
+
+export const UIProvider = ReactUtil.memo("UIProvider", ({locale, children}: Props) => {
+    useDidMountEffect(() => LocaleUtil.setInitial(locale));
+    const currentLocale = LocaleUtil.current();
+    return (
+        <StyleProvider hashPriority="high" transformers={[legacyLogicalPropertiesTransformer]}>
+            <LocaleContext.Provider value={currentLocale}>
+                <ConfigProvider locale={currentLocale === "zh" ? chineseLocale : englishLocale}>{children}</ConfigProvider>
+            </LocaleContext.Provider>
+        </StyleProvider>
+    );
+});
