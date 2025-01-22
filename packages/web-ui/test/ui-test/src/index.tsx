5e67f07c93ddbc615f1d4c96502ba63d87c6f444
uay13211 <david19961113@gmail.com>
2025-01-22 15:33:18 +0800

    [REFACTOR]: web-ui: update ui-test

--- packages/web-ui/test/ui-test/src/index.tsx
+++ packages/web-ui/test/ui-test/src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {Main} from "./component/Main";
+import "./index.less";
 
 ReactDOM.createRoot(document.querySelector("#app-root")!).render(<Main />);
