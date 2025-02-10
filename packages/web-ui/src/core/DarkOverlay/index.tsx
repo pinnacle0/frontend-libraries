4803b0c532aec31df63ed34b08c2a555cb2f62c0
uay13211 <david19961113@gmail.com>
2025-02-10 15:46:26 +0800

    [CHORE]: web-ui: add new props to <DrakOverlay />

--- packages/web-ui/src/core/DarkOverlay/index.tsx
+++ packages/web-ui/src/core/DarkOverlay/index.tsx
@@ -6,15 +6,16 @@ export interface Props {
     children: React.ReactNode;
     className?: string;
     style?: React.CSSProperties;
+    onClick?: () => void;
 }
 
 export class DarkOverlay extends React.PureComponent<Props> {
     static displayName = "DarkOverlay";
 
     render() {
-        const {children, className, style} = this.props;
+        const {children, className, style, onClick} = this.props;
         return ReactDOM.createPortal(
-            <div className={`g-dark-overlay ${className || ""}`} style={style}>
+            <div className={`g-dark-overlay ${className || ""}`} style={style} onClick={e => e.target === e.currentTarget && onClick?.()}>
                 {children}
             </div>,
             document.body
