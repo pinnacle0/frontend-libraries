537287d91757fe992f39880c0313a0ab573f1b00
Anthony Lee <anthonylee1994@users.noreply.github.com>
2025-03-19 18:18:04 +0800

    [FIX] web-ui: update to 0.6.10 to fix MessageUtil usage

--- packages/web-ui/src/util/MessageUtil.tsx
+++ packages/web-ui/src/util/MessageUtil.tsx
@@ -1,21 +1,43 @@
-import type React from "react";
+import React from "react";
 import type {ArgsProps} from "antd/es/message";
-import message from "antd/es/message";
+import Message from "antd/es/message";
+import type {MessageInstance} from "antd/es/message/interface";
+
+let messageInstance: MessageInstance | null = null;
 
 function success(content: string | React.ReactElement) {
-    message.success(content);
+    (messageInstance || Message).success(content);
 }
 
 function error(content: string | React.ReactElement) {
-    message.error(content);
+    (messageInstance || Message).error(content);
 }
 
 function info(config: Partial<Omit<ArgsProps, "type">> & {content: ArgsProps["content"]}) {
-    message.open({duration: 3, ...config, type: "info"});
+    (messageInstance || Message).open({duration: 3, ...config, type: "info"});
+}
+
+function Root(): React.ReactElement {
+    const [apiInstance, contextHolder] = Message.useMessage();
+    React.useEffect(
+        () => {
+            if (messageInstance) {
+                throw new Error("[web-ui] Message.Root cannot be mounted more than once");
+            }
+            messageInstance = apiInstance;
+            return () => {
+                messageInstance = null;
+            };
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps -- for didMount/willUnmount lifecycle
+        []
+    );
+    return contextHolder;
 }
 
 export const MessageUtil = Object.freeze({
     success,
     error,
     info,
+    Root,
 });
