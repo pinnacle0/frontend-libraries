509174fa871e3c9792b37797aa2dc3f9a0a94da8
Anthony Lee <anthonylee1994@users.noreply.github.com>
2025-04-24 16:33:17 +0800

    [REMOVAL] web-ui: remove BrowserBookmarkUtil

--- packages/web-ui/src/internal/i18n/util/en.ts
+++ packages/web-ui/src/internal/i18n/util/en.ts
@@ -4,6 +4,4 @@ export const en = {
     defaultConfirm: "Confirm",
     defaultCancel: "Cancel",
     autoClose: "Closing in {1} sec",
-    addBookmarkWithShortcut: "Please press {1} to add to bookmark",
-    addBookmarkManually: "Please add to bookmark manually",
 };
--- packages/web-ui/src/internal/i18n/util/zh.ts
+++ packages/web-ui/src/internal/i18n/util/zh.ts
@@ -4,6 +4,4 @@ export const zh = {
     defaultConfirm: "确定",
     defaultCancel: "取消",
     autoClose: "{1} 秒后自动关闭",
-    addBookmarkWithShortcut: "请使用快捷键 {1} 添加到收藏夹",
-    addBookmarkManually: "请手动添加到收藏夹",
 };
