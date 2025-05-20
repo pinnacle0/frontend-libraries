7900fe6ef32f49f64aa390a2be2516c02138f0e8
uay13211 <david19961113@gmail.com>
2025-05-20 17:27:17 +0800

    [REFACTOR]: web-ui: add css to hide loading icon in Button

--- packages/web-ui/src/core/Button/index.tsx
+++ packages/web-ui/src/core/Button/index.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import AntButton from "antd/es/button";
 import type {ButtonProps} from "antd/es/button";
 import classNames from "classnames";
+import "./index.less";
 
 export interface Props extends ButtonProps {
     loading?: boolean;
 }
 
 export function Button({className, loading, style, ...rest}: Props) {
-    return <AntButton className={classNames("g-button", className)} style={{gap: 0, ...style}} loading={loading && {icon: <React.Fragment />}} {...rest} />;
+    return <AntButton className={classNames("g-button", className)} loading={loading} {...rest} />;
 }
 
 export type {ButtonSize, ButtonShape, ButtonType, ButtonHTMLType} from "antd/es/button";
