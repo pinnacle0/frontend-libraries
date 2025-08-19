3c78a11b29ecc391bf5b56e97f879a74d27d4006
ianlau <ianlauyin@gmail.com>
2025-08-19 13:59:02 +0800

    [REFACTOR] util: simplify DecoratorUtil

--- packages/util/src/core/DecoratorUtil.ts
+++ packages/util/src/core/DecoratorUtil.ts
@@ -1,36 +1,29 @@
 type Fn<Args extends readonly any[], Return> = (...args: Args) => Return;
 type ReplaceFn<Args extends readonly any[], Return> = (fn: Fn<Args, Return>, ...args: Args) => Return;
 
-// TODO/Ian: add optional, rename to functionIntercept
-interface ReplaceFunctionOptions<Args extends readonly any[], Return> {
-    sync: ReplaceFn<Args, Return>;
-    async: ReplaceFn<Args, Promise<Return>>;
-}
-
-function replaceFunction<Args extends readonly any[], Return>(replaceFn: ReplaceFunctionOptions<Args, Return>) {
-    function factory(fn: Fn<Args, Promise<Return>>): Fn<Args, Promise<Return>>;
-    function factory(fn: Fn<Args, Return>): Fn<Args, Return>;
-
-    function factory(fn: Fn<Args, Return | Promise<Return>>): Fn<Args, Return | Promise<Return>> {
-        if (isAsyncFunction(fn)) {
-            return (...args: Args): Promise<Return> => replaceFn.async(fn, ...args);
-        } else if (isSyncFunction(fn)) {
-            return (...args: Args): Return => replaceFn.sync(fn, ...args);
-        }
-        throw new Error("Invalid function type");
-    }
-
-    return factory;
-}
-
-function isAsyncFunction<Args extends readonly any[], Return>(fn: Fn<Args, Return | Promise<Return>>): fn is Fn<Args, Promise<Return>> {
-    return fn.constructor.name === "AsyncFunction";
-}
-
-function isSyncFunction<Args extends readonly any[], Return>(fn: Fn<Args, Return | Promise<Return>>): fn is Fn<Args, Return> {
-    return fn.constructor.name === "Function";
+/**
+ * Replace function with a new function with type safe.
+ * You can defined Args and Return type for decorated function.
+ *
+ * @example
+ * ```ts
+ * const replaceFn = DecoratorUtil.interceptFunction<[arg1: string, arg2:number], number>((fn, arg1, arg2) => {
+ *     // do something before the function is called
+ *     return fn(...args); // number
+ * });
+ * ```
+ *
+ * @typeParam Args - The arguments of the function. default is `any[]`.
+ * @typeParam Return - The return type of the function. default is `any`.
+ * @param replaceFn - The function to replace the original function.
+ * @returns The decorator factory function.
+ */
+function interceptFunction<Args extends readonly any[] = any[], Return = any>(replaceFn: ReplaceFn<Args, Return>) {
+    return (fn: Fn<Args, Return>) =>
+        (...args: Args) =>
+            replaceFn(fn, ...args);
 }
 
 export const DecoratorUtil = Object.freeze({
-    replaceFunction,
+    interceptFunction,
 });
--- packages/util/test/core/DecoratorUtil.test.ts
+++ packages/util/test/core/DecoratorUtil.test.ts
@@ -1,15 +1,49 @@
-import {describe, test} from "vitest";
+import {describe, test, expect} from "vitest";
+import {DecoratorUtil} from "../../src/core/DecoratorUtil.js";
 
-describe("DecoratorUtil.replaceFunction", () => {
-    test.skip("sync function", () => {
-        // TODO/Ian: add test
-    });
+const AddNumberBefore = (number: number) => DecoratorUtil.interceptFunction<[number], number>((fn, originalNumber) => fn(originalNumber + number));
+const AddNumberBeforeAsync = (number: number) => DecoratorUtil.interceptFunction<[number], Promise<number>>(async (fn, originalNumber) => await fn(originalNumber + number));
 
-    test.skip("async function", () => {
-        // TODO/Ian: add test
-    });
+describe("DecoratorUtil.interceptFunction", () => {
+    test("type safe", async () => {
+        class TestClass {
+            @AddNumberBefore(1)
+            instanceAddOneAndmultiply(x: number): number {
+                return x * 2;
+            }
 
-    test.skip("both sync and async function", () => {
-        // TODO/Ian: add test
+            @AddNumberBefore(1)
+            static staticAddOneAndmultiply(x: number): number {
+                return x * 2;
+            }
+
+            @AddNumberBeforeAsync(1)
+            static async addOneAndmultiplyAsync(x: number): Promise<number> {
+                return x * 2;
+            }
+
+            // @ts-expect-error
+            @AddNumberBeforeAsync(1)
+            static addOneAndmultiplyMixed(x: number): number {
+                return x * 2;
+            }
+
+            // @ts-expect-error
+            @AddNumberBefore(1)
+            static numberToString(x: number): string {
+                return x.toString();
+            }
+
+            // @ts-expect-error
+            @AddNumberBefore(1)
+            static stringToNumber(x: string): number {
+                return Number(x);
+            }
+        }
+
+        const testClass = new TestClass();
+        expect(testClass.instanceAddOneAndmultiply(5)).toBe(12);
+        expect(TestClass.staticAddOneAndmultiply(5)).toBe(12);
+        await expect(TestClass.addOneAndmultiplyAsync(5)).resolves.toBe(12);
     });
 });
