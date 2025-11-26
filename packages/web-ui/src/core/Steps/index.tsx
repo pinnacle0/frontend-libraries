7d35532205bbf1e6e2918a47640a4e968937b1db
ianlau <ianlauyin@gmail.com>
2025-11-26 16:07:59 +0800

    [REFACTOR] web-ui: update antd to 6.0.0

--- packages/web-ui/src/core/Steps/index.tsx
+++ packages/web-ui/src/core/Steps/index.tsx
@@ -1,10 +1,10 @@
-import type {StepsProps, StepProps} from "antd/es/steps";
+import type {StepsProps} from "antd/es/steps";
 import AntSteps from "antd/es/steps";
 import React from "react";
 
 export interface Props extends StepsProps {}
 
-export type StepItem = StepProps;
+export type StepItem = NonNullable<StepsProps["items"]>[number];
 
 export class Steps extends React.PureComponent<Props> {
     static displayName = "Steps";
