299a93df3cca7213af29a6ec49487f12f8f1c221
ianlau <ianlauyin@gmail.com>
2025-11-27 09:38:31 +0800

    [REFACTOR] web-ui: update Tag default variant

--- packages/web-ui/src/core/Tag/index.tsx
+++ packages/web-ui/src/core/Tag/index.tsx
@@ -2,15 +2,11 @@ import React from "react";
 import type {TagProps} from "antd/es/tag";
 import AntTag from "antd/es/tag";
 import type {PresetColorType, PresetStatusColorType} from "antd/es/_util/colors";
-
-export interface Props extends TagProps {}
-
-export class Tag extends React.PureComponent<Props> {
-    static displayName = "Tag";
-
-    render() {
-        return <AntTag {...this.props} />;
-    }
-}
+import {ReactUtil} from "../../util/ReactUtil";
 
 export type TagColor = PresetColorType | PresetStatusColorType;
+export interface Props extends TagProps {}
+
+export const Tag = ReactUtil.memo("Tag", ({variant = "outlined", ...props}: Props) => {
+    return <AntTag variant={variant} {...props} />;
+});
