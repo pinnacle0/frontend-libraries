1b612172fa6e1cd81d9df2f91c1aa76122f50888
ianlau <ianlauyin@gmail.com>
2025-11-27 11:13:28 +0800

    [TODO] web-ui: update TODO

--- packages/web-ui/src/core/VirtualTable/index.tsx
+++ packages/web-ui/src/core/VirtualTable/index.tsx
@@ -16,7 +16,7 @@ import "./index.less";
 
 export type {VirtualTableColumn, VirtualTableRowSelection} from "./type";
 
-// TODO/Ian: replace with Antd virtual table
+// TODO/Ian: remove after this is replace with AntTable visual
 export interface VirtualTableProps<RowType extends object> {
     dataSource: RowType[];
     columns: VirtualTableColumn<RowType>[];
