d2be76fdc608561181aeaa81b6c3f1f167e86599
ianlau <ianlauyin@gmail.com>
2025-11-27 10:47:08 +0800

    [REFACTOR] web-ui: rewrite AutoCompleteInput into functional component

--- packages/web-ui/src/core/AutoCompleteInput/index.tsx
+++ packages/web-ui/src/core/AutoCompleteInput/index.tsx
@@ -1,15 +1,12 @@
 import type {AutoCompleteProps} from "antd/es/auto-complete";
 import AntAutoComplete from "antd/es/auto-complete";
 import React from "react";
+import {ReactUtil} from "../../util/ReactUtil";
 
 export interface Props extends AutoCompleteProps {}
 
-export class AutoCompleteInput extends React.PureComponent<Props> {
-    static displayName = "AutoCompleteInput";
+export const AutoCompleteInputOption: typeof AntAutoComplete.Option = AntAutoComplete.Option;
 
-    static Option: typeof AntAutoComplete.Option = AntAutoComplete.Option;
-
-    render() {
-        return <AntAutoComplete {...this.props} />;
-    }
-}
+export const AutoCompleteInput = ReactUtil.memo("AutoCompleteInput", (props: Props) => {
+    return <AntAutoComplete {...props} />;
+});
