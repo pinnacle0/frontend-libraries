78dbdf00384b5706aecbfd4f154b5f1032ee41eb
ianlau <ianlauyin@gmail.com>
2025-12-09 10:49:02 +0800

    [REFACTOR] web-ui: update VirtualTable

--- packages/web-ui/src/core/VirtualTable/index.tsx
+++ packages/web-ui/src/core/VirtualTable/index.tsx
@@ -17,13 +17,36 @@ export const VirtualTable = ReactUtil.memo("VirtualTable", function <RowType ext
     const {dataSource, className, rowKey = "index", height: propHeight, width: propWidth, emptyPlaceholder, emptyNodeStyle, ...restProps} = props;
     const [height, setHeight] = React.useState<number | "100%">(propHeight ?? "100%");
     const [width, setWidth] = React.useState<number | "100%">(propWidth ?? "100%");
+    const [headerHeight, setHeaderHeight] = React.useState<number>(0);
     const [scrollX, setScrollX] = React.useState<number>(0);
     const [scrollY, setScrollY] = React.useState<number>(0);
     const containerRef = React.useRef<HTMLDivElement>(null);
 
-    // Need to update height and width to match the parent even height/width is provided since it should not exceed the parent's size
-    const updateSize = React.useCallback(
-        (entries: ResizeObserverEntry[]) => {
+    // Need to listen to header change onMount so we can calculate the scrollY correctly
+    React.useEffect(() => {
+        const header = containerRef.current?.querySelector(".ant-table-header");
+        if (!header) return;
+
+        const observer = new ResizeObserver(entries => {
+            setHeaderHeight(entries[0].contentRect.height);
+        });
+        observer.observe(header);
+        return () => {
+            observer.unobserve(header);
+            observer.disconnect();
+        };
+    }, [setHeaderHeight]);
+
+    React.useEffect(() => {
+        setHeight(propHeight ?? "100%");
+        setWidth(propWidth ?? "100%");
+    }, [propHeight, propWidth]);
+
+    React.useEffect(() => {
+        const parent = containerRef.current?.parentElement;
+        if (!parent) return;
+
+        const observer = new ResizeObserver((entries: ResizeObserverEntry[]) => {
             const {width: parentWidth, height: parentHeight} = entries[0].contentRect;
 
             let newHeight = Math.max(parentHeight, 0);
@@ -34,29 +57,16 @@ export const VirtualTable = ReactUtil.memo("VirtualTable", function <RowType ext
             setHeight(newHeight);
             setWidth(newWidth);
 
-            const headerHeight = containerRef.current?.querySelector(".ant-table-header")?.getBoundingClientRect().height ?? 0;
             setScrollY(newHeight - headerHeight);
             setScrollX(newWidth);
-        },
-        [propHeight, propWidth]
-    );
+        });
 
-    React.useEffect(() => {
-        const parent = containerRef.current?.parentElement;
-        if (!parent) return;
-
-        const observer = new ResizeObserver(updateSize);
         observer.observe(parent);
         return () => {
             observer.unobserve(parent);
             observer.disconnect();
         };
-    }, [updateSize, propHeight, propWidth]);
-
-    React.useEffect(() => {
-        setHeight(propHeight ?? "100%");
-        setWidth(propWidth ?? "100%");
-    }, [propHeight, propWidth]);
+    }, [propHeight, propWidth, headerHeight]);
 
     const containerStyle = React.useMemo(() => ({height, width}), [height, width]);
 
