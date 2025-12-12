8bc0b8908c34b7a7280db2f3a84eb12d01e60d0c
ianlau <ianlauyin@gmail.com>
2025-12-12 08:39:58 +0800

    [REFACTOR] web-ui: update VirtualTable

--- packages/web-ui/src/core/VirtualTable/index.tsx
+++ packages/web-ui/src/core/VirtualTable/index.tsx
@@ -9,19 +9,16 @@ export type {TableRowSelection, TableColumn, TableColumns} from "../Table";
 
 export interface VirtualTableProps<RowType extends object> extends Omit<TableProps<RowType, undefined>, "rowKey" | "scrollX" | "scrollY"> {
     rowKey?: TableProps<RowType, undefined>["rowKey"];
-    height?: number; // height will include header height
-    width?: number;
+    scrollY?: number; // height will include header height
+    scrollX?: number;
 }
 
-interface Dimensions {
-    height: number | "100%";
-    width: number | "100%";
-    headerHeight: number;
-}
+const containerStyle = {height: "100%", width: "100%"};
 
 export const VirtualTable = ReactUtil.memo("VirtualTable", function <RowType extends object>(props: VirtualTableProps<RowType>) {
-    const {dataSource, className, rowKey = "index", height: propHeight, width: propWidth, emptyPlaceholder, emptyNodeStyle, ...restProps} = props;
-    const [{width, height, headerHeight}, setDimensions] = React.useState<Dimensions>({width: propWidth ?? "100%", height: propHeight ?? "100%", headerHeight: 0});
+    const {dataSource, className, rowKey = "index", scrollY: propScrollY, emptyPlaceholder, emptyNodeStyle, ...restProps} = props;
+    const [scrollY, setScrollY] = React.useState(300);
+    const [headerHeight, setHeaderHeight] = React.useState(0);
     const containerRef = React.useRef<HTMLDivElement>(null);
 
     React.useEffect(() => {
@@ -29,14 +26,10 @@ export const VirtualTable = ReactUtil.memo("VirtualTable", function <RowType ext
         if (!parent) return;
 
         const observer = new ResizeObserver((entries: ResizeObserverEntry[]) => {
-            const {width: parentWidth, height: parentHeight} = entries[0].contentRect;
-
-            let newHeight = Math.max(parentHeight, 0);
-            let newWidth = Math.max(parentWidth, 0);
-            if (propHeight) newHeight = Math.min(newHeight, propHeight);
-            if (propWidth) newWidth = Math.min(newWidth, propWidth);
-
-            setDimensions(prev => ({...prev, height: newHeight, width: newWidth}));
+            const parentHeight = entries[0].contentRect.height;
+            let newScrollY = Math.max(0, parentHeight - headerHeight);
+            if (propScrollY) newScrollY = Math.min(newScrollY, propScrollY);
+            setScrollY(newScrollY);
         });
 
         observer.observe(parent);
@@ -44,7 +37,7 @@ export const VirtualTable = ReactUtil.memo("VirtualTable", function <RowType ext
             observer.unobserve(parent);
             observer.disconnect();
         };
-    }, [propHeight, propWidth, headerHeight]);
+    }, [propScrollY, headerHeight]);
 
     // Need to listen to header change onMount so we can calculate the scrollY correctly
     React.useEffect(() => {
@@ -52,17 +45,15 @@ export const VirtualTable = ReactUtil.memo("VirtualTable", function <RowType ext
         if (!header) return;
 
         const observer = new ResizeObserver(entries => {
-            setDimensions(prev => ({...prev, headerHeight: entries[0].contentRect.height}));
+            setHeaderHeight(entries[0].contentRect.height);
         });
         observer.observe(header);
         return () => {
             observer.unobserve(header);
             observer.disconnect();
         };
-    }, [setDimensions]);
+    }, []);
 
-    const scrollY = React.useMemo(() => (typeof height === "number" ? height - headerHeight : 0), [height, headerHeight]);
-    const containerStyle = React.useMemo(() => ({height, width}), [height, width]);
     const combinedEmptyNodeStyle = React.useMemo(() => {
         return {
             ...emptyNodeStyle,
@@ -79,8 +70,7 @@ export const VirtualTable = ReactUtil.memo("VirtualTable", function <RowType ext
                 /**
                  * Antd <Table virtual /> must use number scrollX or number scrollY to work
                  */
-                scrollX={typeof width === "number" ? width : 0}
-                scrollY={typeof height === "number" ? height - headerHeight : 0}
+                scrollY={scrollY}
                 rowKey={rowKey}
                 emptyPlaceholder={emptyPlaceholder || "暂无数据"}
                 emptyNodeStyle={combinedEmptyNodeStyle}
