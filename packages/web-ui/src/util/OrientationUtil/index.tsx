e2b7b856b1a2c0d06c30f8eaec55e4f0e89b3807
dionshi <dion.shi.hk@gmail.com>
2025-12-18 09:40:20 +0800

    [REFACTOR] web-ui: enhance OrientationUtil robustness in case of unsupported API

--- packages/web-ui/src/util/OrientationUtil/index.tsx
+++ packages/web-ui/src/util/OrientationUtil/index.tsx
@@ -1,52 +1,33 @@
-/**
- * Orientation detection utility for mobile and desktop devices.
- *
- * For Android,
- * Uses the ScreenOrientation API for checking the orientation of the screen.
- * For Deprecated browsers, it uses the window.orientation API.
- *
- * @see https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation
- *
- * For iOS 26.0+ PWA, it does not dispatch any event when the orientation changes,
- * For iOS,
- * Only support iOS 14+
- * Use media queries to check the orientation of the screen for iOS.
- *
- * @see https://developer.mozilla.org/en-US/docs/Web/CSS/@media/orientation
- * @see https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_media_queries/Using_media_queries
- */
-
 import {BrowserUtil} from "../BrowserUtil";
 
-const supportScreenOrientationAPI = typeof window.screen.orientation !== "undefined";
-
 export type OrientationType = "portrait" | "landscape";
-
 export type Subscriber = (orientation: OrientationType) => void;
 
-function subscribe(subscriber: Subscriber) {
-    let lastOrientation = current();
+function subscribe(subscriber: Subscriber): (() => void) | undefined {
+    try {
+        let lastOrientation = current();
 
-    const handler = () => {
-        const newOrientation = current();
-        if (newOrientation !== lastOrientation) {
-            lastOrientation = newOrientation;
-            subscriber(newOrientation);
-        }
-    };
+        const handler = () => {
+            const newOrientation = current();
+            if (newOrientation !== lastOrientation) {
+                lastOrientation = newOrientation;
+                subscriber(newOrientation);
+            }
+        };
 
-    if (BrowserUtil.os() === "ios") {
-        const mediaQuery = window.matchMedia("(orientation: portrait)");
-        if (mediaQuery instanceof EventTarget) {
+        if (BrowserUtil.os() === "ios") {
+            const mediaQuery = window.matchMedia("(orientation: portrait)");
             mediaQuery.addEventListener("change", handler);
             return () => mediaQuery.removeEventListener("change", handler);
+        } else if (typeof window.screen.orientation !== "undefined") {
+            window.screen.orientation.addEventListener("change", handler);
+            return () => window.screen.orientation.removeEventListener("change", handler);
+        } else {
+            window.addEventListener("orientationchange", handler);
+            return () => window.removeEventListener("orientationchange", handler, false);
         }
-    } else if (supportScreenOrientationAPI) {
-        window.screen.orientation.addEventListener("change", handler);
-        return () => window.screen.orientation.removeEventListener("change", handler);
-    } else {
-        window.addEventListener("orientationchange", handler);
-        return () => window.removeEventListener("orientationchange", handler, false);
+    } catch {
+        // do nothing in case of no such API support
     }
 }
 
@@ -54,7 +35,7 @@ function current(): OrientationType {
     try {
         if (BrowserUtil.os() === "ios") {
             return window.matchMedia("(orientation: portrait)").matches ? "portrait" : "landscape";
-        } else if (supportScreenOrientationAPI) {
+        } else if (typeof window.screen.orientation !== "undefined") {
             return window.screen.orientation.angle === 0 ? "portrait" : "landscape";
         } else {
             return window.orientation === 0 ? "portrait" : "landscape";
