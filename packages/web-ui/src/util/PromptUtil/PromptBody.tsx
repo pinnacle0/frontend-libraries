3973aa4c03876f55cacadda339a4d3ac42c90035
ianlau <ianlauyin@gmail.com>
2026-01-14 15:29:45 +0800

    [REFACTOR] web-ui: revert PromptBody to Functional Component and fix useRef

--- packages/web-ui/src/util/PromptUtil/PromptBody.tsx
+++ packages/web-ui/src/util/PromptUtil/PromptBody.tsx
@@ -6,58 +6,45 @@ import {Markdown} from "../../core/Markdown";
 import {classNames} from "../ClassNames";
 import type {PromptConfig, PromptResult} from "./index";
 import "./index.less";
+import {ReactUtil} from "../ReactUtil";
+
+export interface PromptBodyHandler {
+    validateForm: () => Promise<PromptResult>;
+}
 
 export interface Props extends PromptConfig {}
 
-interface State {
-    textValue: string;
-}
+const warningStyle: React.CSSProperties = {marginTop: 8, color: "red"};
+const formStyle: React.CSSProperties = {marginTop: 12};
 
-export class PromptBody extends React.PureComponent<Props, State> {
-    static displayName = "PromptBody";
+export const PromptBody = ReactUtil.memo(
+    "PromptBody",
+    React.forwardRef(({body, warning, inputPlaceholder, inputType, initialInputValue, inputValidator}: Props, forwardRef) => {
+        const [textValue, setTextValue] = React.useState(initialInputValue || "");
+        const formRef = React.useRef<FormHandler>(null);
 
-    private readonly formRef: React.RefObject<FormHandler | null>;
-    private readonly warningStyle: React.CSSProperties = {marginTop: 8, color: "red"};
-    private readonly formStyle: React.CSSProperties = {marginTop: 12};
+        React.useImperativeHandle(forwardRef, () => ({
+            validateForm: async (): Promise<PromptResult> => {
+                const validationPassed = await formRef.current!.triggerSubmit();
+                if (validationPassed) return {value: textValue};
+                return null;
+            },
+        }));
 
-    constructor(props: Props) {
-        super(props);
-        this.state = {textValue: props.initialInputValue || ""};
-        this.formRef = React.createRef();
-    }
-
-    validateForm = async (): Promise<PromptResult> => {
-        const validationPassed = this.formRef.current!.triggerSubmit();
-        if (await validationPassed) {
-            return {value: this.state.textValue};
-        } else {
-            return null;
-        }
-    };
-
-    render() {
-        const {body, warning, inputPlaceholder, inputType, inputValidator} = this.props;
-        const {textValue} = this.state;
         return (
             <div className={classNames("g-modal-prompt-body", inputType)}>
                 <Markdown>{body}</Markdown>
-                <p style={this.warningStyle}>{warning}</p>
-                <Form ref={this.formRef} style={this.formStyle} buttonRenderer={null}>
+                <p style={warningStyle}>{warning}</p>
+                <Form ref={formRef} style={formStyle} buttonRenderer={null}>
                     <Form.Item validator={() => inputValidator?.(textValue) || null}>
                         {inputType === "multi-line" ? (
-                            <Input.TextArea placeholder={inputPlaceholder} value={textValue} onChange={textValue => this.setState({textValue})} />
+                            <Input.TextArea placeholder={inputPlaceholder} value={textValue} onChange={setTextValue} />
                         ) : (
-                            <Input
-                                type={inputType === "password" ? "password" : "text"}
-                                autoComplete="new-password"
-                                placeholder={inputPlaceholder}
-                                value={textValue}
-                                onChange={textValue => this.setState({textValue})}
-                            />
+                            <Input type={inputType === "password" ? "password" : "text"} autoComplete="new-password" placeholder={inputPlaceholder} value={textValue} onChange={setTextValue} />
                         )}
                     </Form.Item>
                 </Form>
             </div>
         );
-    }
-}
+    })
+);
