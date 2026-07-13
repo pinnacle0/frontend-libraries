1a0e5cbbc0fca6f2ab7e6813d3030a8b87597d4c
uay13211 <david19961113@gmail.com>
2026-07-13 09:41:50 +0800

    [REFACTOR]: web-ui: update Clipboard.copyText

--- packages/web-ui/src/util/Clipboard/index.tsx
+++ packages/web-ui/src/util/Clipboard/index.tsx
@@ -1,22 +1,27 @@
-function copyText(text: string) {
-    const element = document.createElement("textarea");
-    element.value = text;
-    element.style.position = "absolute";
-    element.style.left = "-9999px";
-    element.setAttribute("readonly", "");
-    document.body.appendChild(element);
+async function copyText(text: string) {
+    // navigator.clipboard supposed to be unavailable only in local http server
+    if (navigator.clipboard) {
+        await navigator.clipboard.writeText(text);
+    } else {
+        const element = document.createElement("textarea");
+        element.value = text;
+        element.style.position = "absolute";
+        element.style.left = "-9999px";
+        element.setAttribute("readonly", "");
+        document.body.appendChild(element);
 
-    // In case there is already some text selection by user, store the previous selection & recover later
-    const selection = document.getSelection();
-    const selected = selection != null && selection.rangeCount > 0 ? selection.getRangeAt(0) : null;
+        // In case there is already some text selection by user, store the previous selection & recover later
+        const selection = document.getSelection();
+        const selected = selection != null && selection.rangeCount > 0 ? selection.getRangeAt(0) : null;
 
-    element.select();
-    document.execCommand("copy");
-    document.body.removeChild(element);
+        element.select();
+        document.execCommand("copy");
+        document.body.removeChild(element);
 
-    if (selection && selected) {
-        selection.removeAllRanges();
-        selection.addRange(selected);
+        if (selection && selected) {
+            selection.removeAllRanges();
+            selection.addRange(selected);
+        }
     }
 }
 
